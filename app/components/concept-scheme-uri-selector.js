7253c4938849286ca0207f3c2cf436f16d201857
Unknown <achrafatauil@hotmail.com>
2021-03-04 17:06:09 +0100

    Add codelist prefLabels when choosing concept scheme

--- app/components/concept-scheme-uri-selector.js
+++ app/components/concept-scheme-uri-selector.js
@@ -7,34 +7,49 @@ const SPARQL_ENDPOINT = "/sparql"
 
 export default class ConceptSchemeUriSelectorComponent extends Component {
 
-  @tracked options = []
   @tracked selected;
+  @tracked options;
+  @tracked codeList;
 
-  query = `
+  constructor() {
+    super(...arguments)
+    this.loadOptions()
+  }
+
+  async loadOptions() {
+    this.options = await this.queryDB(`
     SELECT DISTINCT * {
       ?uri a <http://www.w3.org/2004/02/skos/core#ConceptScheme>;
         <http://www.w3.org/2004/02/skos/core#prefLabel> ?prefLabel.
     }
-  `
-
-  constructor() {
-    super(...arguments)
-    this.getSchemeLabels(this.query)
+  `)
   }
+
+  async loadCodeList() {
+    this.codeList = await this.queryDB(`
+      SELECT DISTINCT * {
+        ?p ?o  <${this.selected.uri.value}>;
+        <http://www.w3.org/2004/02/skos/core#prefLabel> ?prefLabel.
+      }
+      LIMIT 10
+    `)
+  }
+
 
   @action
   setSelected(value){
     this.selected = value
+    this.loadCodeList()
   }
 
-  async getSchemeLabels(query) {
+  async queryDB(query) {
     const encodedQuery = escape(query);
     const endpoint = `${SPARQL_ENDPOINT}?query=${encodedQuery}`;
     const response = await fetch(endpoint, { headers: {'Accept': 'application/sparql-results+json' } });
 
     if (response.ok) {
       let jsonResponds = await response.json();
-      this.options = jsonResponds.results.bindings
+      return jsonResponds.results.bindings
     } else {
       throw new Error(`Request was unsuccessful: [${response.status}] ${response.statusText}`);
     }
