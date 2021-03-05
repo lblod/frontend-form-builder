204088e1b4e748bb4fae7f5abf068e464440595b
Claire Lovisa <lovisa.claire@gmail.com>
2021-03-05 15:40:13 +0100

    [WIP] Display validations

--- app/components/input-types-list.js
+++ app/components/input-types-list.js
@@ -11,30 +11,52 @@ export default class InputTypesListComponent extends Component {
     return this.inputTypes.sort((a,b) => (a.label.value > b.label.value));
   }
 
-  query = `
+  queryInputTypes = `
     PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
 
-    SELECT DISTINCT ?concept ?label ?displayType ?usesConceptScheme {
-      ?concept skos:inScheme <http://lblod.data.gift/concept-schemes/c5a91bd7-3eb5-4d69-a51b-9bac6bf345f6> ;
+    SELECT DISTINCT ?inputType ?label ?displayType ?usesConceptScheme {
+      ?inputType skos:inScheme <http://lblod.data.gift/concept-schemes/c5a91bd7-3eb5-4d69-a51b-9bac6bf345f6> ;
         skos:prefLabel ?label ;
         ext:displayType ?displayType ;
         ext:usesConceptScheme ?usesConceptScheme .
     }
-  `
+  `;
 
-  constructor() {
-    super(...arguments)
-    this.getInputTypes(this.query)
+  queryValidations = function(inputType) {
+    return `
+    PREFIX ext: <http://mu.semte.ch/vocabularies/ext/>
+
+    SELECT DISTINCT ?validation ?label ?validatonName {
+      <${inputType}> ext:canHaveValidation ?validation .
+      ?validation skos:prefLabel ?label ;
+        ext:validationName ?validatonName .
+    }
+  `;
   }
 
-  async getInputTypes(query) {
-    const encodedQuery = escape(query);
-    const endpoint = `${SPARQL_ENDPOINT}?query=${encodedQuery}`;
-    const response = await fetch(endpoint, { headers: {'Accept': 'application/sparql-results+json' } });
+  constructor() {
+    super(...arguments);
+    this.getInputTypes(this.queryInputTypes, this.queryValidations);
+  }
 
-    if (response.ok) {
-      let jsonResponse = await response.json();
-      this.inputTypes = jsonResponse.results.bindings
+  async getInputTypes(queryInputTypes, queryValidations) {
+    const encodedQueryInputTypes = escape(queryInputTypes);
+    const endpointInputTypes = `${SPARQL_ENDPOINT}?query=${encodedQueryInputTypes}`;
+    const responseInputTypes = await fetch(endpointInputTypes, { headers: {'Accept': 'application/sparql-results+json' } });
+    
+    if (responseInputTypes.ok) {
+      let jsonResponseInputTypes = await responseInputTypes.json();
+      this.inputTypes = jsonResponseInputTypes.results.bindings;
+
+      for (const inputType of this.inputTypes) {
+        const encodedQueryValidations = escape(queryValidations(inputType.inputType.value));
+        const endpointValidations = `${SPARQL_ENDPOINT}?query=${encodedQueryValidations}`;
+        const responseValidations = await fetch(endpointValidations, { headers: {'Accept': 'application/sparql-results+json' } });
+
+        let jsonResponseValidations = await responseValidations.json();
+        const validations = jsonResponseValidations.results.bindings;
+        inputType['validations'] = validations;
+      }
     } else {
       throw new Error(`Request was unsuccessful: [${response.status}] ${response.statusText}`);
     }
