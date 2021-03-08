02ea2d90753f45e8e3ab86c8c86fce628b8e33fd
Unknown <achrafatauil@hotmail.com>
2021-03-08 10:44:54 +0100

    seperate save-locally & save-remote buttons for form

--- app/components/playground.js
+++ app/components/playground.js
@@ -3,7 +3,7 @@ import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
 
-import { sym as RDFNode} from 'rdflib';
+import { sym as RDFNode } from 'rdflib';
 import { GRAPHS } from '../controllers/formbuilder';
 import { FORM, RDF } from '../util/rdflib';
 
@@ -14,7 +14,7 @@ export const TEXT_AREA = {
 }
 
 export default class Playground extends Component {
-
+  @service store;
   textarea = TEXT_AREA;
 
   @service('meta-data-extractor') meta;
@@ -35,41 +35,45 @@ export default class Playground extends Component {
   }
 
   @action
-  exportTTL() {
-
-    /*
-    ** SAVE FILE LOCALLY CODE
-    */
-
+  saveLocally() {
     // Create a link
     let downloadLink = document.createElement("a")
     downloadLink.download = "test.ttl"
 
     // generate Blob where file content will exists
-    let blob = new Blob([this.args.template], {type:"text/plain"})
+    let blob = new Blob([this.args.template], { type: "text/plain" })
     downloadLink.href = window.URL.createObjectURL(blob)
 
     // Click file to download then destroy link
     downloadLink.click();
     downloadLink.remove();
+  }
 
+  @action
+  async uploadToDB() {
 
-    /*
-    ** SAVE FILE ON THE SERVER
-    ** important: needs the blob element created above to work
-    */
-
-    // Create a "fake form"
-    let mockForm = new FormData();
-    mockForm.append('file', blob, "schema.ttl")
-
-    fetch('/files', {
-      method: "POST",
-      body: mockForm,
-      redirect: 'follow'
+    const newForm = await this.store.createRecord('generated-form',{
+      created: new Date(),
+      modified: new Date(),
+      label: "form123",
+      comment: "a generated form entity",
+      ttlCode: this.args.template
     })
 
+    newForm.save()
+    // let blob = new Blob([this.args.template], { type: "text/plain" })
 
+    // // Create a "fake form"
+    // let mockForm = new FormData();
+    // mockForm.append('file', blob, "schema.ttl")
 
+    // fetch('/files', {
+    //   method: "POST",
+    //   body: {
+    //     file: mockForm,
+    //     name: "test"
+    //   },
+    //   redirect: 'follow'
+    // })
   }
 }
