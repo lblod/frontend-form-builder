0f5d507e498fa69da8b6e86e873e35d7c215facd
Claire Lovisa <lovisa.claire@gmail.com>
2021-03-08 20:03:33 +0100

    Fix infinite load when refrshing

--- app/routes/user-tests/edit.js
+++ app/routes/user-tests/edit.js
@@ -12,7 +12,7 @@ export default class UserTestsEditRoute extends Route {
 
   async model(params) {
     const test = await this.store.findRecord('user-test', params.id, {
-      include: ['form.ttl-code'].join(','),
+      include: ['form.ttl-code'],
     });
 
     const graphs = GRAPHS;
