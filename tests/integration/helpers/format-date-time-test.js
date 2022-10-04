98014e86461fd56d087702b0fdd4ac2d8d24cf77
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2022-10-04 16:29:48 +0200

    Remove ember-moment (#9)
    This replaces ember-moment with 2 custom helpers.

---
+++ tests/integration/helpers/format-date-time-test.js
@@ -0,0 +1,26 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Helper | format-date-time', function (hooks) {
+  setupRenderingTest(hooks);
+
+  test('it returns a formatted date with time information', async function (assert) {
+    this.date = new Date(2022, 9, 4, 16, 12);
+    await render(hbs`{{format-date-time this.date}}`);
+
+    assert.dom().hasText('4/10/2022 16:12');
+  });
+
+  test('it returns an empty string if the provided value is not a date', async function (assert) {
+    await render(hbs`{{format-date-time this.date}}`);
+    assert.dom().hasText('');
+
+    this.set('date', null);
+    assert.dom().hasText('');
+
+    this.set('date', '');
+    assert.dom().hasText('');
+  });
+});
