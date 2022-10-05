58e63e2ebd86eae63d5b9cec4db9c485373916dc
Sam Van Campenhout <sam.van.campenhout@gmail.com>
2022-10-05 11:34:56 +0200

    Don't serialize the `uri` attribute
    mu-cl-resources considers `uri` a read-only attribute so it throws an error if it's included in the payload

--- app/models/user-test.js
+++ app/models/user-test.js
@@ -1,15 +1,8 @@
 /* eslint-disable ember/no-test-import-export */ // False positive since the file has a `-test` suffix
 import Model, { attr, belongsTo } from '@ember-data/model';
 
-const RESOURCE_BASE = 'http://data.lblod.info/user-tests/';
-
 export default class UserTestModel extends Model {
-  /**
-   * NOTE: using `@attr('string') uri;` causes PATCH to fail on resource
-   */
-  get uri() {
-    return `${RESOURCE_BASE}${this.id}`;
-  }
+  @attr uri;
 
   @attr('date', {
     defaultValue() {
