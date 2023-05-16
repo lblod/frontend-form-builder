912be7789768921d42622af53a7b94d28c5d477d
Achraf Atauil <achrafatauil@hotmail.com>
2023-05-16 15:36:52 +0200

    setup editor (not functional yet)

---
+++ app/modifiers/editor.js
@@ -0,0 +1,29 @@
+import { EditorView, basicSetup } from 'codemirror';
+import { turtle } from 'codemirror-lang-turtle';
+
+import { modifier } from 'ember-modifier';
+
+export default modifier(function editor(parent, [value, setEditor, onChange]) {
+  const extensions = [basicSetup, turtle()];
+  const doc = value || '';
+
+  if (typeof onChange === 'function') {
+    const updateListener = EditorView.updateListener.of((viewUpdate) => {
+      if (viewUpdate.docChanged && typeof onChange === 'function') {
+        const doc = viewUpdate.state.doc;
+        const value = doc.toString();
+        onChange(value, viewUpdate);
+      }
+    });
+
+    extensions.push(updateListener);
+  }
+
+  const editor = new EditorView({
+    parent,
+    doc,
+    extensions,
+  });
+
+  setEditor(editor);
+});
