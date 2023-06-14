d4c137a5e390beffc6aff9e70038b46a451e17cf
Achraf Atauil <achrafatauil@hotmail.com>
2023-06-14 16:20:05 +0200

    add linter & eventlistener to codemirror

--- app/modifiers/editor.js
+++ app/modifiers/editor.js
@@ -1,27 +1,52 @@
 import { EditorView, basicSetup } from 'codemirror';
 import { turtle } from 'codemirror-lang-turtle';
+import {syntaxTree} from "@codemirror/language"
+import {linter, lintGutter} from '@codemirror/lint'
 
 import { modifier } from 'ember-modifier';
 
+function simpleLinter() {
+  return linter(view => {
+    const {state} = view
+    const tree = syntaxTree(state)
+    if (tree.length === state.doc.length) {
+      let pos = null
+      tree.iterate({enter: n => {
+        if (pos == null && n.type.isError) {
+          pos = n.from
+          return false
+        }
+      }})
+
+      if (pos != null)
+        return [{from: pos, to: pos+1, severity: 'error', message: 'syntax error'}]
+    } 
+
+    return []
+  })
+}
+
 export default modifier(function editor(parent, [value, onChange]) {
-  const extensions = [basicSetup, turtle()];
+  const extensions = [basicSetup, turtle(), simpleLinter(), lintGutter()];
   const doc = value || '';
 
-  if (typeof onChange === 'function') {
-    const updateListener = EditorView.updateListener.of((viewUpdate) => {
-      if (viewUpdate.docChanged && typeof onChange === 'function') {
-        const doc = viewUpdate.state.doc;
-        const value = doc.toString();
-        onChange(value, viewUpdate);
-      }
-    });
+  const updateListener = EditorView.updateListener.of((viewUpdate) => {
+    if (viewUpdate.docChanged) {
+      const doc = viewUpdate.state.doc;
+      const value = doc.toString();
+      onChange.perform(value)
+    }
+  });
 
-    extensions.push(updateListener);
-  }
+  extensions.push(updateListener);
 
   const editor = new EditorView({
     parent,
     doc,
     extensions,
   });
+
+  return () => {
+    editor.destroy()
+  }
 });
