8565a305ff7e1c86097e7c627716ba334033fd93
Achraf Atauil <achrafatauil@hotmail.com>
2023-06-19 12:25:23 +0200

    try to integrate visual builder (WIP)

--- app/components/playground.js
+++ app/components/playground.js
@@ -6,8 +6,12 @@ import { tracked } from '@glimmer/tracking';
 import { sym as RDFNode } from 'rdflib';
 import { GRAPHS } from '../controllers/formbuilder/edit';
 import { FORM, RDF } from '../utils/rdflib';
+import { trackedFunction } from 'ember-resources/util/function';
+import { ForkingStore } from '@lblod/ember-submission-form-fields';
+import { task } from 'ember-concurrency';
 
 const SOURCE_NODE = new RDFNode('http://frontend.poc.form.builder/sourcenode');
+const META_SOURCE_NODE = new RDFNode('http://frontend.poc.form.builder/meta/sourcenode');
 
 export const TEXT_AREA = {
   id: 'playground-text-area',
@@ -25,6 +29,13 @@ export default class Playground extends Component {
   @tracked saved = false;
   @tracked error = false;
   @tracked currentView = "CODE";
+  @tracked forkingStore;
+
+  metaGraphs = {
+    formGraph: new RDFNode('http://data.lblod.info/meta/form'),
+    metaGraph: new RDFNode('http://data.lblod.info/meta/metagraph'),
+    sourceGraph: new RDFNode(`http://data.lblod.info/meta/sourcegraph`),
+  };
 
   constructor() {
     super(...arguments);
@@ -33,9 +44,37 @@ export default class Playground extends Component {
      * Statics
      */
     this.node = SOURCE_NODE;
+    this.metaNode = META_SOURCE_NODE;
     this.graphs = GRAPHS;
     this.args.refresh.perform();
+    this.refreshMeta.perform();
   }
+
+  @task
+  *refreshMeta(value) {
+    let formRes = yield fetch(`/forms/form.ttl`);
+    let form = yield formRes.text();
+
+    let metaRes = yield fetch(`/forms/meta.ttl`);
+    let meta = yield metaRes.text();
+
+    this.forkingStore = new ForkingStore();
+    this.forkingStore.parse(
+      form,
+      GRAPHS.formGraph.value,
+      'text/turtle'
+    );
+
+    this.forkingStore.parse(meta, GRAPHS.metaGraph.value, 'text/turtle');
+    this.forkingStore.parse(this.args.template, GRAPHS.sourceGraph.value, 'text/turtle');
+
+    sourceTtl = this.forkingStore.serializeDataMergedGraph(GRAPHS.sourceGraph);
+  };
+
+  metaStore = trackedFunction(this, async () => {
+    const store = new forkingStore()
+    return store
+  });
 
   get form() {
     return this.args.store.any(
