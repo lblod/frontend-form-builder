503ebad5bf0a65f52d36b1dc9116a1429195cdfd
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2023-08-17 14:49:23 +0200

    refactor: make the code more readable

--- app/routes/formbuilder/edit.js
+++ app/routes/formbuilder/edit.js
@@ -3,6 +3,7 @@ import template from '../../utils/basic-form-template';
 import { inject as service } from '@ember/service';
 import { registerFormFields } from '@lblod/ember-submission-form-fields';
 import PropertyGroupSelector from '../../components/rdf-form-fields/property-group-selector';
+import { tracked } from '@glimmer/tracking';
 
 export default class FormbuilderEditRoute extends Route {
   @service store;
@@ -12,15 +13,28 @@ export default class FormbuilderEditRoute extends Route {
     this.registerCustomFields();
   }
 
-  model(params) {
-    return this.store.findRecord('generated-form', params.id);
+  async model(params) {
+    return await this.getForm(params.id);
   }
 
-  setupController(controller, model) {
+  async setupController(controller, model) {
     super.setupController(...arguments);
     controller.refresh.perform({
-      value: model.ttlCode ? model.ttlCode : template,
+      value: this.getFormTtlCode(model),
     });
+  }
+
+  async getForm(generatedFormId) {
+    try {
+      const form = await this.store.findRecord(
+        'generated-form',
+        generatedFormId
+      );
+
+      return form;
+    } catch (error) {
+      throw `Could not fetch generated-form with id: ${generatedFormId}`;
+    }
   }
 
   registerCustomFields() {
@@ -31,5 +45,13 @@ export default class FormbuilderEditRoute extends Route {
         edit: PropertyGroupSelector,
       },
     ]);
+  }
+
+  getFormTtlCode(model) {
+    if (!model.ttlCode || model.ttlCode == '') {
+      return template;
+    }
+
+    return model.ttlCode;
   }
 }
