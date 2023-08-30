6029fdd770a569a72a8feeaa35d1cfa898bdf637
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2023-08-30 14:28:31 +0200

    wip: add a switch to toggle between the builder and the validations tab
    - show  list of all the Fields with their containing validations
    - try removing all validations from a field
    - try adding a validation RequiredConstrained to a field
    
    Refs: #DL-5289

--- app/controllers/formbuilder/edit.js
+++ app/controllers/formbuilder/edit.js
@@ -4,8 +4,13 @@ import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import { task, timeout } from 'ember-concurrency';
 import { inject as service } from '@ember/service';
-import { ForkingStore } from '@lblod/ember-submission-form-fields';
-import { sym as RDFNode } from 'rdflib';
+import {
+  ForkingStore,
+  getTopLevelPropertyGroups,
+  getChildrenForPropertyGroup,
+  validationTypesForField,
+} from '@lblod/ember-submission-form-fields';
+import { parse, sym as RDFNode } from 'rdflib';
 import { FORM, RDF } from '../../utils/rdflib';
 
 export const GRAPHS = {
@@ -29,12 +34,128 @@ export default class FormbuilderEditController extends Controller {
   @tracked builderForm;
 
   @tracked formChanged = false;
+  @tracked isAddingValidationToForm = false;
+
+  localeMetaTtlContentAsText = '';
+
+  @tracked fieldsInForm = [];
+
+  @action
+  toggleIsAddingValidationToForm() {
+    this.set('isAddingValidationToForm', !this.isAddingValidationToForm);
+    if (!this.isAddingValidationToForm) {
+      console.log('perform refresh');
+      this.refresh.perform({
+        formTtlCode: this.code,
+        resetBuilder: false,
+        isInitialRouteCall: true,
+      });
+    } else {
+      this.getAllFieldsInTheForm();
+    }
+  }
 
   graphs = GRAPHS;
   sourceNode = SOURCE_NODE;
   REGISTERED_FORM_TTL_CODE_KEY = 'formTtlCode';
 
   @tracked isInitialDataLoaded = false;
+
+  @action
+  async getAllFieldsInTheForm() {
+    this.fieldsInForm = [];
+    if (this.code == '') {
+      return null;
+    }
+
+    const fieldStore = new ForkingStore();
+    fieldStore.parse(this.code, GRAPHS.formGraph.value, 'text/turtle');
+    const metaTtl = await this.getMetaTtlContent();
+    fieldStore.parse(metaTtl, GRAPHS.metaGraph.value, 'text/turtle');
+
+    const fieldForm = this.previewStore.any(
+      undefined,
+      RDF('type'),
+      FORM('Form'),
+      GRAPHS.formGraph
+    );
+
+    const propertyGroups = getTopLevelPropertyGroups({
+      store: fieldStore,
+      graphs: this.graphs,
+      form: fieldForm,
+    });
+    for (const group of propertyGroups) {
+      const children = getChildrenForPropertyGroup(group, {
+        form: fieldForm,
+        store: fieldStore,
+        graphs: this.graphs,
+        sourceNode: this.sourceNode,
+      });
+
+      for (const field of children) {
+        const validationTypes = validationTypesForField(field.uri, {
+          store: fieldStore,
+          formGraph: this.graphs.formGraph,
+        });
+        this.fieldsInForm.push({
+          parent: field,
+          fieldName: field.rdflibLabel,
+          uri: field.uri.value,
+          validationTypes: validationTypes,
+          store: fieldStore,
+          fieldForm: fieldForm,
+        });
+        console.log({ validationTypes });
+      }
+    }
+  }
+
+  @action
+  addIsRequiredValidationToField(field) {
+    console.log('addIsRequiredValidationToField');
+    console.log({ field });
+  }
+
+  @action
+  removeAllValidationsFromField(field) {
+    console.log('this.code', this.code);
+    console.log({ field });
+    const validationsInTheForm =
+      field.store.graph.predicateIndex[
+        '<http://lblod.data.gift/vocabularies/forms/validations>'
+      ];
+    console.log({ validationsInTheForm });
+    const statementsToRemove = validationsInTheForm.filter((validation) => {
+      return validation.subject.value == field.uri;
+    });
+
+    console.log({ statementsToRemove });
+
+    for (const statement of statementsToRemove) {
+      field.store.removeMatches(
+        statement.subject,
+        statement.predicate,
+        statement.object,
+        statement.graph
+      );
+    }
+
+    console.log('Field store', field.store);
+    // this.serializeSourceToTtl();
+
+    this.builderStore = field.store;
+
+    console.log(
+      '1',
+      this.builderStore.graph.statements.includes(statementsToRemove[0])
+    );
+    console.log(
+      '2',
+      this.builderStore.graph.statements.includes(statementsToRemove[1])
+    );
+    console.log('updated the validations of the field');
+  }
 
   @task({ restartable: true })
   *refresh({ formTtlCode, resetBuilder, isInitialRouteCall = false }) {
@@ -65,7 +186,7 @@ export default class FormbuilderEditController extends Controller {
     );
 
     const formTtl = yield this.getLocalFileContentAsText('/forms/form.ttl');
-    const metaTtl = yield this.getLocalFileContentAsText('/forms/meta.ttl');
+    const metaTtl = yield this.getMetaTtlContent();
 
     this.builderStore = new ForkingStore();
     this.builderStore.parse(formTtl, GRAPHS.formGraph.value, 'text/turtle');
@@ -85,6 +206,7 @@ export default class FormbuilderEditController extends Controller {
 
     if (isInitialRouteCall) {
       this.setFormChanged(false);
+      this.isAddingValidationToForm = false;
     }
 
     this.isInitialDataLoaded = true;
@@ -109,6 +231,19 @@ export default class FormbuilderEditController extends Controller {
     const file = await fetch(path);
 
     return await file.text();
+  }
+
+  async getMetaTtlContent() {
+    if (
+      !this.localeMetaTtlContentAsText ||
+      this.localeMetaTtlContentAsText == ''
+    ) {
+      this.localeMetaTtlContentAsText = await this.getLocalFileContentAsText(
+        '/forms/meta.ttl'
+      );
+    }
+
+    return this.localeMetaTtlContentAsText;
   }
 
   deregisterFromObservable() {
