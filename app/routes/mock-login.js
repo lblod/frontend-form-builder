b89237e360f44086b4a6529ab855e774cf8bc978
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2023-09-26 09:08:36 +0200

    feat: return the accounts that match the filter from the user
    Refs: #SFB-19

--- app/routes/mock-login.js
+++ app/routes/mock-login.js
@@ -19,20 +19,13 @@ export default class MockLoginRoute extends Route {
     const filter = { provider: 'https://github.com/lblod/mock-login-service' };
     if (params.gemeente) filter.gebruiker = { achternaam: params.gemeente };
 
-    const separteAllAccounts = await this.store.findAll('account', {});
-    const separteAllGebruikers = await this.store.findAll('gebruiker', {});
-    console.log('separate all accounts', [...separteAllAccounts]);
-    console.log('separate all gebruikers', [...separteAllGebruikers]);
-    // const account = await this.store.query('account', {
-    //   include: 'gebruiker.bestuurseenheden',
-    //   filter: filter,
-    //   page: { size: 10, number: params.page },
-    //   sort: 'gebruiker.achternaam',
-    // });
+    const accounts = await this.store.query('account', {
+      include: 'gebruiker.bestuurseenheden',
+      filter: filter,
+      page: { size: 10, number: params.page },
+      sort: 'gebruiker.achternaam',
+    });
 
-    // console.log('account: ', [...account]);
-
-    // return account;
-    return null;
+    return accounts;
   }
 }
