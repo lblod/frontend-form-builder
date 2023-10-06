e226d4bd9d38deea3df4991d62b6682d3f32f774
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2023-10-06 10:46:53 +0200

    feat: show the fields in as separate rdf forms in the validation tab (preview broken)
    Refs: #SFB-3

--- app/components/add-validations-to-form.js
+++ app/components/add-validations-to-form.js
@@ -5,13 +5,14 @@ import { task } from 'ember-concurrency';
 import { GRAPHS } from '../controllers/formbuilder/edit';
 import { ForkingStore } from '@lblod/ember-submission-form-fields';
 import { getLocalFileContentAsText } from '../utils/get-local-file-content';
-import { FORM, RDF } from '../utils/rdflib';
+import { FORM, RDF, EXT, EMBER, DISPLAY } from '../utils/rdflib';
 import { sym as RDFNode } from 'rdflib';
 import { areValidationsInGraphValidated } from '../utils/validation-shape-validators';
 
 export default class AddValidationsToFormComponent extends Component {
   @tracked builderStore;
   @tracked builderForm;
+  @tracked storesWithForm;
 
   graphs = {
     ...GRAPHS,
@@ -24,6 +25,7 @@ export default class AddValidationsToFormComponent extends Component {
     if (!this.args.formTtlCode || this.args.formTtlCode == '') {
       throw `Cannot add validations to an empty form`;
     }
+    this.storesWithForm = [];
 
     this.initialise.perform({ formTtlCode: this.args.formTtlCode });
   }
@@ -42,9 +44,9 @@ export default class AddValidationsToFormComponent extends Component {
       this.graphs.formGraph
     );
 
-    this.builderStore.registerObserver(() => {
-      this.serializeToTtlCode(this.builderStore);
-    });
+    // this.builderStore.registerObserver(() => {
+    //   this.serializeToTtlCode(this.builderStore);
+    // });
   }
 
   willDestroy() {
@@ -82,6 +84,126 @@ export default class AddValidationsToFormComponent extends Component {
     );
     builderStore.parse(formTtlCode, this.graphs.sourceGraph, 'text/turtle');
 
+    this.storesWithForm = await this.createSeparateStorePerField(builderStore);
+
+    console.log('storesWithForm', this.storesWithForm);
+
     return builderStore;
   }
+
+  async createSeparateStorePerField(store) {
+    const triplesPerFieldInForm = this.getTriplesPerFieldInForm(store);
+    const storesWithForm = [];
+
+    for (const field of triplesPerFieldInForm) {
+      const fieldStore = new ForkingStore();
+      fieldStore.addAll(field.triples);
+
+      const ttl = fieldStore.serializeDataMergedGraph(this.graphs.sourceGraph);
+      fieldStore.parse(something, this.graphs.sourceGraph, 'text/turtle');
+
+      await this.parseStoreGraphs(fieldStore, ttl);
+
+      storesWithForm.push({
+        displayType: field.displayType,
+        store: fieldStore,
+        form: fieldStore.any(
+          undefined,
+          RDF('type'),
+          FORM('Form'),
+          this.graphs.formGraph
+        ),
+      });
+    }
+
+    return storesWithForm;
+  }
+
+  async parseStoreGraphs(store, ttlCode) {
+    store.parse(
+      await getLocalFileContentAsText('/forms/validation/form.ttl'),
+      this.graphs.formGraph,
+      'text/turtle'
+    );
+    store.parse(
+      await getLocalFileContentAsText('/forms/validation/meta.ttl'),
+      this.graphs.metaGraph,
+      'text/turtle'
+    );
+    store.parse(
+      await getLocalFileContentAsText('/forms/builder/meta.ttl'),
+      this.graphs.fieldGraph,
+      'text/turtle'
+    );
+    store.parse(ttlCode, this.graphs.sourceGraph, 'text/turtle');
+  }
+
+  getTriplesPerFieldInForm(store) {
+    const possibleFieldSubjects = store
+      .match(
+        EMBER('source-node'),
+        FORM('includes'),
+        undefined,
+        this.graphs.sourceGraph
+      )
+      .map((triple) => triple.object);
+
+    if (possibleFieldSubjects.length == 0) {
+      throw `No fields found in form.`;
+    }
+
+    const triplesPerField = [];
+
+    for (const fieldSubject of possibleFieldSubjects) {
+      const fieldTriples = store.match(
+        fieldSubject,
+        undefined,
+        undefined,
+        this.graphs.sourceGraph
+      );
+
+      const displayTypeTriple = fieldTriples.find(
+        (triple) => triple.predicate.value == FORM('displayType').value
+      );
+      if (!displayTypeTriple) {
+        throw `Could not find display type for field`;
+      }
+      const tripleIsField = fieldTriples.find(
+        (triple) => triple.object.value == FORM('Field').value
+      );
+      if (tripleIsField) {
+        triplesPerField.push({
+          displayType: displayTypeTriple.object,
+          triples: fieldTriples,
+        });
+      }
+    }
+
+    return triplesPerField;
+  }
 }
+
+const something = `@prefix form: <http://lblod.data.gift/vocabularies/forms/> .
+@prefix sh: <http://www.w3.org/ns/shacl#> .
+@prefix mu: <http://mu.semte.ch/vocabularies/core/> .
+@prefix displayTypes: <http://lblod.data.gift/display-types/> .
+@prefix ext: <http://mu.semte.ch/vocabularies/ext/> .
+@prefix schema: <http://schema.org/> .
+@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+@prefix nie: <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#> .
+@prefix fieldGroups: <http://data.lblod.info/field-groups/> .
+@prefix fields: <http://data.lblod.info/fields/> .
+@prefix concept: <http://lblod.data.gift/concept-schemes/> .
+
+##########################################################
+# Form
+##########################################################
+ext:form a form:Form, form:TopLevelForm ;
+  form:includes ext:formNodesL .
+
+##########################################################
+#  Property-groups
+##########################################################
+ext:formFieldPg a form:PropertyGroup;
+  sh:name "" ;
+  sh:order 1 .`;
--- app/utils/rdflib.js
+++ app/utils/rdflib.js
@@ -6,6 +6,9 @@ import { Namespace } from 'rdflib';
 export const RDF = new Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#');
 export const FORM = new Namespace('http://lblod.data.gift/vocabularies/forms/');
 export const FIELDS = new Namespace('http://data.lblod.info/fields/');
+export const EXT = new Namespace('http://mu.semte.ch/vocabularies/ext/');
+export const DISPLAY = new Namespace('http://lblod.data.gift/display-types/');
+export const EMBER = new Namespace('http://ember-submission-form-fields/');
 export const FIELD_GROUPS = new Namespace(
   'http://data.lblod.info/field-groups/'
 );
