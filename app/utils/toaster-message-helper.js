4e663bed704e2e967a1cc459bd4f8db12017c01f
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2023-10-13 12:29:48 +0200

    fix: lint in toaster message helper
    Refs: #SFB-8

--- app/utils/toaster-message-helper.js
+++ app/utils/toaster-message-helper.js
@@ -1,4 +1,8 @@
-export function showErrorToasterMessage(toasterService, message, timeInMs = 5000) {
+export function showErrorToasterMessage(
+  toasterService,
+  message,
+  timeInMs = 5000
+) {
   toasterService.error(message, 'Error', {
     timeOut: timeInMs,
   });
