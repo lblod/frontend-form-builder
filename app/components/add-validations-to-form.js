56571c31c8f865d47e5675fd59bb01e56bd25de2
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2023-10-26 11:09:46 +0200

    refactor: move the merge fields forms with builder form top separate method
    Refs: #SFB-44

--- app/components/add-validations-to-form.js
+++ app/components/add-validations-to-form.js
@@ -59,62 +59,61 @@ export default class AddValidationsToFormComponent extends Component {
     this.registerToObservableForStoresWithForm(this.storesWithForm);
   }
 
-  async mergeThFieldFormsWithTheBuilderForm() {
-    this.deregisterFromObservableForStoresWithForm(this.storesWithForm);
+  async mergeFieldDataWithBuilderForm(fieldData) {
+    const storeWithMergedField = await mergeFieldDataWithBuilderForm(
+      fieldData,
+      this.savedBuilderTtlCode,
+      this.graphs
+    );
 
-    for (const fieldData of this.getFieldsData(this.storesWithForm)) {
-      const storeWithMergedField = await mergeFieldDataWithBuilderForm(
-        fieldData,
-        this.savedBuilderTtlCode,
-        this.graphs
-      );
-
-      const sourceTtl = storeWithMergedField.serializeDataMergedGraph(
-        this.graphs.sourceGraph
-      );
-      this.savedBuilderTtlCode = sourceTtl;
-    }
+    const sourceTtl = storeWithMergedField.serializeDataMergedGraph(
+      this.graphs.sourceGraph
+    );
+    this.savedBuilderTtlCode = sourceTtl;
 
     this.args.onUpdateValidations(this.savedBuilderTtlCode);
+  }
+
+  getFieldDataForStoreWithForm(storeWithForm) {
+    const isValidTtl = areValidationsInGraphValidated(
+      storeWithForm.store,
+      this.graphs.sourceGraph
+    );
+    if (isValidTtl) {
+      const triples = getFieldAndValidationTriples(
+        storeWithForm.subject,
+        storeWithForm.store,
+        this.graphs.sourceGraph
+      );
+
+      return {
+        store: storeWithForm.store,
+        subject: storeWithForm.subject,
+        triples: triples,
+      };
+    } else {
+      showErrorToasterMessage(
+        this.toaster,
+        `Form of field with subject: ${storeWithForm.subject} is invalid.`
+      );
+      console.error(
+        `Current invalid field ttl for subject: ${storeWithForm.subject}`,
+        storeWithForm.store.serializeDataMergedGraph(this.graphs.sourceGraph)
+      );
+    }
   }
 
   getFieldsData(storesWithForm) {
     const fieldsData = [];
 
     for (const storeWithForm of storesWithForm) {
-      const isValidTtl = areValidationsInGraphValidated(
-        storeWithForm.store,
-        this.graphs.sourceGraph
-      );
-      if (isValidTtl) {
-        const triples = getFieldAndValidationTriples(
-          storeWithForm.subject,
-          storeWithForm.store,
-          this.graphs.sourceGraph
-        );
-
-        fieldsData.push({
-          store: storeWithForm.store,
-          subject: storeWithForm.subject,
-          triples: triples,
-        });
-      } else {
-        showErrorToasterMessage(
-          this.toaster,
-          `Form of field with subject: ${storeWithForm.subject} is invalid.`
-        );
-        console.error(
-          `Current invalid field ttl for subject: ${storeWithForm.subject}`,
-          storeWithForm.store.serializeDataMergedGraph(this.graphs.sourceGraph)
-        );
-      }
+      fieldsData.push(this.getFieldDataForStoreWithForm(storeWithForm));
     }
 
     return fieldsData;
   }
 
-  @task({ restartable: true })
-  *updatedFormFieldValidations(builderStore) {
+  updatedFormFieldValidations(builderStore) {
     if (
       !areValidationsInGraphValidated(builderStore, this.graphs.sourceGraph)
     ) {
@@ -170,8 +169,16 @@ export default class AddValidationsToFormComponent extends Component {
       builderStore.addAll([statement]);
     }
 
-    yield this.mergeThFieldFormsWithTheBuilderForm();
-    this.registerToObservableForStoresWithForm()
+    const storeWithForm = this.storesWithForm
+      .filter((storeWithForm) => storeWithForm.store == builderStore)
+      .shift();
+    this.mergeFieldDataWithBuilderForm(
+      this.getFieldDataForStoreWithForm(storeWithForm)
+    );
+
+    builderStore.registerObserver(() => {
+      this.updatedFormFieldValidations(builderStore);
+    });
   }
 
   updateDifferencesInTriples(newTriples, oldTriples, store) {
@@ -243,10 +250,10 @@ export default class AddValidationsToFormComponent extends Component {
     }
   }
 
-  registerToObservableForStoresWithForm() {
-    for (const storeWithForm of this.storesWithForm) {
-      storeWithForm.store.registerObserver(async () => {
-        await this.updatedFormFieldValidations.perform(storeWithForm.store);
+  registerToObservableForStoresWithForm(storesWithForm) {
+    for (const storeWithForm of storesWithForm) {
+      storeWithForm.store.registerObserver(() => {
+        this.updatedFormFieldValidations(storeWithForm.store);
       });
     }
   }
