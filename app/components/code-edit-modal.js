641259e0a8f54ae675a01d6074aab43cf319b43e
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2023-10-27 10:17:28 +0200

    feat: add reset + save button to the code editor
    Refs: #SFB-46

--- app/components/code-edit-modal.js
+++ app/components/code-edit-modal.js
@@ -1,9 +1,42 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 import { restartableTask, timeout } from 'ember-concurrency';
 
 export default class CodeEditModal extends Component {
+  @tracked formCode;
+  @tracked formCodeUpdates;
+  @tracked isButtonsDisabled;
+
+  constructor() {
+    super(...arguments);
+    this.formCode = this.args.code;
+    this.isButtonsDisabled = true;
+  }
+
   handleCodeChange = restartableTask(async (newCode) => {
-    await timeout(500);
-    this.args.onCodeChange?.(newCode);
+    if (newCode == this.formCode) {
+      this.isButtonsDisabled = true;
+      this.formCode = this.formCodeUpdates;
+
+      return;
+    }
+    this.formCodeUpdates = newCode;
+    this.isButtonsDisabled = false;
   });
+
+  @action
+  restoreForm() {
+    this.isButtonsDisabled = true;
+    this.formCode = this.args.code;
+  }
+
+  @action
+  updateForm() {
+    this.isButtonsDisabled = true;
+    this.formCode = this.formCodeUpdates;
+    console.log(`update code to `, this.formCode);
+    this.args.onCodeChange?.(this.formCode);
+    this.args.onClose(true);
+  }
 }
