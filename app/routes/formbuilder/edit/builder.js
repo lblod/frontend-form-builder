e57842827af04624d7c8834042d887b4ca4876fc
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2023-11-06 13:16:55 +0100

    SFB-75: create route js/template files for the builder and the validations

---
+++ app/routes/formbuilder/edit/builder.js
@@ -0,0 +1,7 @@
+import Route from '@ember/routing/route';
+
+export default class FormbuilderEditBuilderRoute extends Route {
+  constructor() {
+    super(...arguments);
+  }
+}
---
+++ app/routes/formbuilder/edit/validations.js
@@ -0,0 +1,7 @@
+import Route from '@ember/routing/route';
+
+export default class FormbuilderEditValidationsRoute extends Route {
+  constructor() {
+    super(...arguments);
+  }
+}
