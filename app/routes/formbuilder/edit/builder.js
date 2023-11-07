74c2c732d11c0f5a13fb1ed6a3aeac1f3ffcc678
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2023-11-07 09:26:06 +0100

    SFB-75: move the route js files to the correct directly

--- app/routes/formbuilder/edit/builder.js
+++ app/routes/formbuilder/edit/builder.js
@@ -3,5 +3,6 @@ import Route from '@ember/routing/route';
 export default class FormbuilderEditBuilderRoute extends Route {
   constructor() {
     super(...arguments);
+    console.log(`Builder router constructor`);
   }
 }
--- app/routes/formbuilder/edit/validations.js
+++ app/routes/formbuilder/edit/validations.js
@@ -3,5 +3,6 @@ import Route from '@ember/routing/route';
 export default class FormbuilderEditValidationsRoute extends Route {
   constructor() {
     super(...arguments);
+    console.log(`Validations router constructor`);
   }
 }
