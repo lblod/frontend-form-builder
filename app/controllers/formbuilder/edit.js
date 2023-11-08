3455e290e5b54061fdb58127dc3c9d2f58560a8a
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2023-11-08 11:05:35 +0100

    SFB-75: remove code that has been moved to a separate controller

--- app/controllers/formbuilder/edit.js
+++ app/controllers/formbuilder/edit.js
@@ -2,11 +2,8 @@ import Controller from '@ember/controller';
 
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
-import { restartableTask, timeout } from 'ember-concurrency';
 import { inject as service } from '@ember/service';
-import { ForkingStore } from '@lblod/ember-submission-form-fields';
 import { sym as RDFNode } from 'rdflib';
-import { FORM, RDF } from '../../utils/rdflib';
 import basicFormTemplate from '../../utils/basic-form-template';
 
 export const GRAPHS = {
@@ -15,44 +12,13 @@ export const GRAPHS = {
   sourceGraph: new RDFNode(`http://data.lblod.info/sourcegraph`),
 };
 
-const SOURCE_NODE = new RDFNode('http://frontend.poc.form.builder/sourcenode');
-
 export default class FormbuilderEditController extends Controller {
   @service store;
   @service('form-code-manager') formCodeManager;
 
   @tracked formCode;
 
-  @tracked previewStore;
-  @tracked previewForm;
-
   @tracked formChanged = false;
-
-  graphs = GRAPHS;
-  sourceNode = SOURCE_NODE;
-
-  setupPreviewForm = restartableTask(async () => {
-    // force a component recreation by unrendering it very briefly
-    // Ideally the RdfForm component would do the right thing when the formStore
-    // and form arguments change, but we're not there yet.
-    await timeout(1);
-    this.formCodeManager.addFormCode(this.formCode);
-    this.setFormChanged(this.formCodeManager.isLatestDeviatingFromReference());
-
-    this.previewStore = new ForkingStore();
-    this.previewStore.parse(
-      this.formCode,
-      GRAPHS.formGraph.value,
-      'text/turtle'
-    );
-
-    this.previewForm = this.previewStore.any(
-      undefined,
-      RDF('type'),
-      FORM('Form'),
-      GRAPHS.formGraph
-    );
-  });
 
   @action
   setFormChanged(value) {
@@ -69,7 +35,6 @@ export default class FormbuilderEditController extends Controller {
     this.formCode = this.getFormTtlCode(model.generatedForm);
     this.formCodeManager.addFormCode(this.formCode);
     this.formCodeManager.pinLatestVersionAsReference();
-    this.setupPreviewForm.perform();
   }
 
   reset() {
