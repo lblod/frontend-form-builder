43bf6af0d913c981efdbd51c0d68580bd780fbf9
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2023-11-13 10:50:53 +0100

    SFB-71: Update concept-scheme-uri-selector to use the utils query function

--- app/components/concept-scheme-uri-selector.js
+++ app/components/concept-scheme-uri-selector.js
@@ -1,8 +1,7 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-
-const SPARQL_ENDPOINT = '/sparql';
+import { queryDB } from '../utils/query-sparql-query';
 
 export default class ConceptSchemeUriSelectorComponent extends Component {
   @tracked selected;
@@ -14,7 +13,7 @@ export default class ConceptSchemeUriSelectorComponent extends Component {
   }
 
   async loadOptions() {
-    this.options = await this.queryDB(`
+    this.options = await queryDB(`
     SELECT DISTINCT * {
       ?uri a <http://www.w3.org/2004/02/skos/core#ConceptScheme>;
         <http://www.w3.org/2004/02/skos/core#prefLabel> ?prefLabel.
@@ -23,7 +22,7 @@ export default class ConceptSchemeUriSelectorComponent extends Component {
   }
 
   async update() {
-    let concepts = await this.queryDB(`
+    let concepts = await queryDB(`
       SELECT DISTINCT ?prefLabel {
         ?s <http://www.w3.org/2004/02/skos/core#inScheme> <${this.selected.uri.value}>;
         <http://www.w3.org/2004/02/skos/core#prefLabel> ?prefLabel.
@@ -40,22 +39,5 @@ export default class ConceptSchemeUriSelectorComponent extends Component {
   setSelected(value) {
     this.selected = value;
     this.update();
-  }
-
-  async queryDB(query) {
-    const encodedQuery = escape(query);
-    const endpoint = `${SPARQL_ENDPOINT}?query=${encodedQuery}`;
-    const response = await fetch(endpoint, {
-      headers: { Accept: 'application/sparql-results+json' },
-    });
-
-    if (response.ok) {
-      let jsonResponds = await response.json();
-      return jsonResponds.results.bindings;
-    } else {
-      throw new Error(
-        `Request was unsuccessful: [${response.status}] ${response.statusText}`
-      );
-    }
   }
 }
