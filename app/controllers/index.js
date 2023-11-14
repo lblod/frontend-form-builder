78a4f377dbb0ffbfc5749d5ca8227b38942276b2
Achraf Atauil <achrafatauil@hotmail.com>
2023-11-14 18:19:44 +0100

    remove setup and set size to 20

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,22 +1,10 @@
 import Controller from '@ember/controller';
-
 import { inject as service } from '@ember/service';
-import { tracked } from '@glimmer/tracking';
 
 export default class IndexController extends Controller {
   @service store;
 
   sort = '-created';
   page = 0;
-  size = 5;
-
-  @tracked generatedForms;
-
-  async setup() {
-    const allForms = await this.store.query('generated-form', {
-      count: true,
-    });
-    this.model.meta.count = allForms.length;
-    this.generatedForms = this.model;
-  }
+  size = 20;
 }
