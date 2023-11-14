0c5fd0328de845dd303975504f90c9dcbb8ad275
Achraf Atauil <achrafatauil@hotmail.com>
2023-11-14 18:24:50 +0100

    remove unneeded store injection for controllers

--- app/controllers/user-tests/index.js
+++ app/controllers/user-tests/index.js
@@ -1,9 +1,6 @@
 import Controller from '@ember/controller';
-import { inject as service } from '@ember/service';
 
 export default class UserTestsIndexController extends Controller {
-  @service store;
-
   sort = '-created';
   page = 0;
   size = 20;
