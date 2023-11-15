98889405b5662b8b06ab6e6cb72c00a1ecfa8aa0
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2023-11-15 16:41:11 +0100

    SFB-72: Create the view in html and css

--- app/controllers/formbuilder/edit/create-codelist.js
+++ app/controllers/formbuilder/edit/create-codelist.js
@@ -4,12 +4,81 @@ import { restartableTask, timeout } from 'ember-concurrency';
 import { tracked } from '@glimmer/tracking';
 import { ForkingStore } from '@lblod/ember-submission-form-fields';
 import { FORM, RDF } from '../../../utils/rdflib';
+import { action } from '@ember/object';
+import { A } from '@ember/array';
 
 export default class FormbuilderEditCreateCodelistController extends Controller {
   @tracked builderStore;
   @tracked builderForm;
 
   @tracked formTtlCode;
+
+  @tracked listName;
+  @tracked newItem;
+  @tracked listItems;
+  @tracked generatedCodelistTtl;
+  @tracked selectedItem;
+
+  @action
+  addItemToList() {
+    if (!this.listItems) {
+      this.listItems = A([]);
+    }
+    this.listItems.pushObject({
+      name: this.newItem,
+    });
+    this.newItem = null;
+    this.createCodeListTtlForItemList();
+  }
+
+  @action
+  updateListName(event) {
+    this.listName = event.target.value;
+  }
+
+  @action
+  updateNewItemValue(event) {
+    this.newItem = event.target.value;
+  }
+
+  @action
+  setSelected(item) {
+    this.selectedItem = item;
+  }
+
+  createCodeListTtlForItemList() {
+    const prefixes = `
+    @prefix rdf:	<http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
+    @prefix skos:	<http://www.w3.org/2004/02/skos/core#> .
+    @prefix form: <http://lblod.data.gift/vocabularies/forms/> .
+    @prefix rdfs:	<http://www.w3.org/2000/01/rdf-schema#> .
+    @prefix con:	<http://lblod.data.gift/concept-schemes/> .
+    @prefix ext:	<http://mu.semte.ch/vocabularies/ext/> .
+    @prefix sh: <http://www.w3.org/ns/shacl#>.
+    `;
+
+    const ttlItems = this.listItems.map((listItem) => {
+      return this.createItemTtl(listItem.name);
+    });
+
+    this.generatedCodelistTtl = `
+      ${prefixes}
+
+      con:${this.listName.replace(' ', '-')} a skos:ConceptScheme ;
+      skos:prefLabel  "${this.listName}" .
+
+      ${ttlItems.join(' \n')}
+    `;
+  }
+
+  createItemTtl(itemName) {
+    return `
+    ext:customListItem${itemName.replace(' ', '-')}
+      rdf:type  rdfs:Class , skos:Concept ;
+      skos:inScheme	con:${this.listName.replace(' ', '-')} ;
+      skos:prefLabel  "${itemName}" .
+    `;
+  }
 
   setup() {
     this.setupBuilderForm.perform();
