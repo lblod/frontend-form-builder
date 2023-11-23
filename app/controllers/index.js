e3ef770ccad358a1390f956580069058ca688ec5
Arne Kerremans <arnie@pop-os.localdomain>
2023-11-23 15:47:27 +0100

    moving name input to index

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,31 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class IndexController extends Controller {
   sort = '-created';
   page = 0;
   size = 20;
+
+  @service store;
+  @tracked showModal = false;
+  @tracked name = "";
+  @tracked description = ``;
+
+  modelName = 'generated-form';
+
+  @action
+  handleNameChange(event) {
+    this.name = event.target.value;
+  }
+
+  @action
+  handleCommentChange(event) {
+    this.comment = event.target.value;
+  }
+
+  get emptyName() {
+    return this.name == '';
+  }
 }
