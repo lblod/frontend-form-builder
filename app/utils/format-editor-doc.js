73144645102e085536bf3c4a937046b7eb577d15
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2023-11-29 13:54:18 +0100

    SFB-65: format validations blank nodes

--- app/utils/format-editor-doc.js
+++ app/utils/format-editor-doc.js
@@ -1,6 +1,7 @@
 import { getDocWithNewLineAfterDots } from './code-editor-format-rules/add-new-line-after-dot';
 import { getDocWithTabBeforeLineEndingOnDotAndPreviousLineEndsWithSemiColon } from './code-editor-format-rules/add-tab-before-line-ending-with-dot-and-previous-with-semicolon';
 import { getDocWithTabBeforeLineWithSemicolon } from './code-editor-format-rules/add-tab-before-line-with-semicolon';
+import { getDocWithFormattedValidations } from './code-editor-format-rules/format-blankNode-validations';
 import { getDocWithAllSpacesRemovedFromEachLine } from './code-editor-format-rules/remove-all-spaces-from-each-line';
 
 export async function getFormattedEditorCode(doc) {
@@ -10,6 +11,8 @@ export async function getFormattedEditorCode(doc) {
       doc
     );
   doc = await getDocWithNewLineAfterDots(doc);
+  doc = await getDocWithTabBeforeLineWithSemicolon(doc);
+  doc = await getDocWithFormattedValidations(doc);
 
-  return await getDocWithTabBeforeLineWithSemicolon(doc);
+  return doc;
 }
