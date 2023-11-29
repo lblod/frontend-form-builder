cf7068d4f9e431f072f6b83b7ca77428458bea91
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2023-11-29 15:38:43 +0100

    SFB-65: parse the lines that inlclude semicolons and end on a dot

--- app/utils/format-editor-doc.js
+++ app/utils/format-editor-doc.js
@@ -2,6 +2,7 @@ import { getDocWithNewLineAfterDots } from './code-editor-format-rules/add-new-l
 import { getDocWithTabBeforeLineEndingOnDotAndPreviousLineEndsWithSemiColon } from './code-editor-format-rules/add-tab-before-line-ending-with-dot-and-previous-with-semicolon';
 import { getDocWithTabBeforeLineWithSemicolon } from './code-editor-format-rules/add-tab-before-line-with-semicolon';
 import { getDocWithFormattedValidations } from './code-editor-format-rules/format-blankNode-validations';
+import { getDocWithParsedLinesThatEndOnDotAndIncludeSemicolons } from './code-editor-format-rules/parse-line-when-ending-on-dot-and-semicolon-included';
 import { getDocWithAllSpacesRemovedFromEachLine } from './code-editor-format-rules/remove-all-spaces-from-each-line';
 
 export async function getFormattedEditorCode(doc) {
@@ -13,6 +14,7 @@ export async function getFormattedEditorCode(doc) {
   doc = await getDocWithNewLineAfterDots(doc);
   doc = await getDocWithTabBeforeLineWithSemicolon(doc);
   doc = await getDocWithFormattedValidations(doc);
+  doc = await getDocWithParsedLinesThatEndOnDotAndIncludeSemicolons(doc);
 
   return doc;
 }
