4ee24840908038ef2b3a811c0f4b180037ad8bf9
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2023-12-05 15:42:34 +0100

    SFB-65: do not use context but before as text

--- app/utils/code-editor/completion/complete-word.js
+++ app/utils/code-editor/completion/complete-word.js
@@ -4,9 +4,10 @@ export function completeWord(context) {
   let before = context.matchBefore(/\w+/);
 
   if (!context.explicit && !before) return null;
+
   return {
     from: before ? before.from : context.pos,
     options: dictionary,
-    validFor: /\\b${context.text}\\b/, // This has to be better somehow
+    validFor: /\\b${before.text}\\b/,
   };
 }
