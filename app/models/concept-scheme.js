905f63ca0afcccf913ede2ec422c7ecd0ad69b5b
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2023-12-13 09:52:17 +0100

    SFB-100: setting this on true will fix the warnings thrown that data in the relationship is not available

--- app/models/concept-scheme.js
+++ app/models/concept-scheme.js
@@ -3,7 +3,7 @@ import Model, { attr, hasMany } from '@ember-data/model';
 export default class ConceptSchemeModel extends Model {
   @attr uri;
   @attr preflabel;
-  @hasMany('concept', { inverse: null, async: false }) concepts;
+  @hasMany('concept', { inverse: null, async: true }) concepts;
 
   get label() {
     return this.preflabel;
--- app/models/concept.js
+++ app/models/concept.js
@@ -3,7 +3,7 @@ import Model, { attr, hasMany } from '@ember-data/model';
 export default class ConceptModel extends Model {
   @attr uri;
   @attr preflabel;
-  @hasMany('concept-scheme', { inverse: null, async: false }) conceptSchemes;
+  @hasMany('concept-scheme', { inverse: null, async: true }) conceptSchemes;
 
   get label() {
     return this.preflabel;
