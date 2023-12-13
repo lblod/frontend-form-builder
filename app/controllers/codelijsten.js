ce7a3f1480a769ab4326c26325be64c3adc74d19
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2023-12-13 10:14:29 +0100

    SFB-100: update styling of codelijsten screen

--- app/controllers/codelijsten.js
+++ app/controllers/codelijsten.js
@@ -5,11 +5,20 @@ import { tracked } from '@glimmer/tracking';
 export default class CodelijstenController extends Controller {
   @tracked scheme;
 
-  @action update(scheme) {
+  @action updateSelectedSchemeTo(scheme) {
     this.scheme = scheme;
   }
 
   @action previousRoute() {
     window.history.back();
+  }
+
+  get configurationCode() {
+    const jsonConfigCode = {
+      conceptScheme: this.scheme.uri,
+      searchEnabled: true,
+    };
+
+    return JSON.stringify(jsonConfigCode, null, 2).trim();
   }
 }
