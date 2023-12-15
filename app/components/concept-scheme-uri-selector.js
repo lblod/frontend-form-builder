d213504b750dda1fb64ec001af8f6d6f3f5a5c00
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2023-12-15 11:23:56 +0100

    SFB-73: remove unused import

--- app/components/concept-scheme-uri-selector.js
+++ app/components/concept-scheme-uri-selector.js
@@ -2,10 +2,7 @@ import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-import {
-  triplesForPath,
-  updateSimpleFormValue,
-} from '@lblod/submission-form-helpers';
+import { updateSimpleFormValue } from '@lblod/submission-form-helpers';
 import { Literal } from 'rdflib';
 import { getTriplesWithNodeAsSubject } from '../utils/forking-store-helpers';
 import { FORM } from '../utils/rdflib';
