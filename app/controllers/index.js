32a868ce320459c506817327a55d9020aaffb978
Arne Kerremans <arnie@pop-os.localdomain>
2023-12-18 16:27:26 +0100

    added a remove button on the forms index

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,42 @@
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { inject as service } from '@ember/service';
 
 export default class IndexController extends Controller {
+  @service store;
+  @service toaster;
+
   sort = '-created';
   page = 0;
   size = 20;
+
+  selectedForm;
+
+  @tracked showDeleteModal = false;
+
+  @action
+  OpenDeleteModal(generatedForm) {
+    this.selectedForm = generatedForm;
+    this.showDeleteModal = true;
+  }
+
+  @action
+  async deleteForm() {
+    const formToDelete = await this.store.peekRecord(
+      'generated-form',
+      this.selectedForm.id
+    );
+    const isDeleted = await formToDelete.destroyRecord();
+    if (isDeleted) {
+      this.showDeleteModal = false;
+      this.toaster.success(
+        'Formulier: ' + this.selectedForm.label + ' verwijderd',
+        'Success',
+        {
+          timeOut: 5000,
+        }
+      );
+    }
+  }
 }
