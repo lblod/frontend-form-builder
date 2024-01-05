2c73853ae7ac16120408c7ba0f01210a2dc8ee69
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-01-05 11:54:40 +0100

    SFB-113: create test that checks that nothing changes when the cleanup happens on ttl with field that has one validation

--- tests/unit/utils/clean-up-ttl/remove-all-duplicate-validations-test.js
+++ tests/unit/utils/clean-up-ttl/remove-all-duplicate-validations-test.js
@@ -1,6 +1,7 @@
 import { getTtlWithDuplicateValidationsRemoved } from 'frontend-form-builder/utils/clean-up-ttl/remove-all-duplicate-validations';
 import { module, test } from 'qunit';
 import basicFormWithoutValidations from './resources/basic-form-without-validations';
+import basicFormWithOneValidationOnField from './resources/basic-form-with-one-validation-on-field';
 
 module('Unit | Utility | Clean up ttl | Duplicate validations', function () {
   module('Will not remove anything', function () {
@@ -11,6 +12,16 @@ module('Unit | Utility | Clean up ttl | Duplicate validations', function () {
       assert.deepEqual(
         stringToArray(updatedTtlCode),
         stringToArray(basicFormWithoutValidations),
+        'The updated ttl code is the same as the original'
+      );
+    });
+    test('one validation on field', function (assert) {
+      const updatedTtlCode = getTtlWithDuplicateValidationsRemoved(
+        basicFormWithOneValidationOnField
+      );
+      assert.deepEqual(
+        stringToArray(updatedTtlCode),
+        stringToArray(basicFormWithOneValidationOnField),
         'The updated ttl code is the same as the original'
       );
     });
