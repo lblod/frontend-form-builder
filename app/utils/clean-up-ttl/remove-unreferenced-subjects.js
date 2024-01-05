a2c75f56da82da2767f25b6dc587ea4443be1656
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-01-05 15:18:57 +0100

    SFB-112: toaster is a warning + add warn log in console for unreferenced

--- app/utils/clean-up-ttl/remove-unreferenced-subjects.js
+++ app/utils/clean-up-ttl/remove-unreferenced-subjects.js
@@ -8,17 +8,24 @@ export function getTtlWithUnReferencedSubjectsRemoved(ttlCode, toaster) {
   const store = new ForkingStore();
   store.parse(ttlCode, GRAPHS.sourceGraph, 'text/turtle');
 
+  const unreferenced = [];
   for (const subject of getAllUniqueSubjectsInStore(store)) {
     if (isSubjectReferenced(subject, store)) {
       continue;
     }
-
-    toaster.error(
+    unreferenced.push(subject.value);
+    toaster.warning(
       `Subject is not referenced: ${subject.value ?? undefined}`,
       'Error',
       {
         timeOut: 5000,
       }
+    );
+  }
+  if (unreferenced.length !== 0) {
+    console.warn(
+      `These subjects are unreferenced in the ttlcode`,
+      unreferenced
     );
   }
 
