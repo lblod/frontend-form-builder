c9364fe26aaa6831a63e41a3134c4096ed5b1bba
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-01-08 16:41:18 +0100

    eat: create helper object + transform exisiting helper methods to object methods

---
+++ app/utils/forking-store-helper.js
@@ -0,0 +1,35 @@
+import { FORM, RDF, SH } from './rdflib';
+import { SKOS } from '@lblod/submission-form-helpers';
+import { ForkingStore } from '@lblod/ember-submission-form-fields';
+
+export const ForkingStoreHelper = {
+  isForkingStore: (store) => {
+    return store instanceof ForkingStore;
+  },
+  getTriplesWithNodeAsSubject: (node, store, graph) => {
+    return store.match(node, undefined, undefined, graph);
+  },
+  getNodeValidationTriples: (node, store, graph) => {
+    return store.match(node, FORM('validations'), undefined, graph);
+  },
+  getValidationSubjectsOnNode: (node, store, graph) => {
+    return store
+      .match(node, FORM('validations'), undefined, graph)
+      .map((triple) => triple.object);
+  },
+  getDisplayTypeOfNode: (node, store, graph) => {
+    return store.any(node, FORM('displayType'), undefined, graph);
+  },
+  getRdfTypeOfNode: (node, store, graph) => {
+    return store.any(node, RDF('type'), undefined, graph);
+  },
+  getGroupingTypeOfNode: (node, store, graph) => {
+    return store.any(node, FORM('grouping'), undefined, graph);
+  },
+  getFirstPathOfNode: (node, store, graph) => {
+    return store.any(node, SH('path'), undefined, graph);
+  },
+  getPrefLabelOfNode: (node, store, graph) => {
+    return store.any(node, SKOS('prefLabel'), undefined, graph);
+  },
+};
