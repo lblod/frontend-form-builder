4572227bca9cc1f23489a1ddb86ba50d9bc812a9
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-01-09 09:14:28 +0100

    refactor: cleanup namespaces ./rdflib

--- app/utils/rdflib.js
+++ app/utils/rdflib.js
@@ -1,14 +1,6 @@
 import { Namespace } from 'rdflib';
 
-/**
- * Namespaces
- */
 export const RDF = new Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#');
 export const FORM = new Namespace('http://lblod.data.gift/vocabularies/forms/');
 export const SH = new Namespace('http://www.w3.org/ns/shacl#');
 export const EXT = new Namespace('http://mu.semte.ch/vocabularies/ext/');
-export const FIELDS = new Namespace('http://data.lblod.info/fields/');
-export const EMBER = new Namespace('http://ember-submission-form-fields/');
-export const FIELD_GROUPS = new Namespace(
-  'http://data.lblod.info/field-groups/'
-);
