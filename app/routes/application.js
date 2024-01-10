56d75b24bcfa4eaa412f0c27b4d770e9f637ea68
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-01-10 14:48:05 +0100

    SFb-116: set locale to en-us

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,7 +1,15 @@
 import Route from '@ember/routing/route';
 import { action } from '@ember/object';
+import { service } from '@ember/service';
 
 export default class FormsPlaygroundRoute extends Route {
+  @service intl; // Inject the intl service
+
+  beforeModel() {
+    // Set the locale before the model is loaded (you can adjust the locale as needed)
+    this.intl.setLocale(['en-us']);
+  }
+
   @action
   willTransition(transition) {
     /* eslint ember/no-controller-access-in-routes: "warn" */
