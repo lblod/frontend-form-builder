3144b819b25d24ee1cc444fb5517a880bf813e47
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-01-10 16:29:27 +0100

    SFB-118: add validations for components

--- app/components/create-form-modal.js
+++ app/components/create-form-modal.js
@@ -12,6 +12,7 @@ export default class CreateFormModal extends Component {
   @service store;
   @service router;
   @service toaster;
+  @service intl;
 
   @tracked name = '';
   @tracked duplicateNames = [];
@@ -48,13 +49,21 @@ export default class CreateFormModal extends Component {
       await newForm.save();
       await newForm.save();
       this.router.transitionTo('formbuilder.edit', newForm.id);
-      this.toaster.success('Formulier succesvol aangemaakt', 'Success', {
-        timeOut: 5000,
-      });
+      this.toaster.success(
+        this.intl.t('messages.success.formCreated'),
+        'Success',
+        {
+          timeOut: 5000,
+        }
+      );
     } catch (err) {
-      this.toaster.error('Oeps, er is iets mis gegaan', 'Error', {
-        timeOut: 5000,
-      });
+      this.toaster.error(
+        this.intl.t('messages.error.couldNotUpdateForm'),
+        'Error',
+        {
+          timeOut: 5000,
+        }
+      );
       console.error(err);
     }
     this.closeModal();
@@ -62,10 +71,10 @@ export default class CreateFormModal extends Component {
 
   get inputErrorMessage() {
     if (this.name.trim() == '' && this.hasBeenFocused) {
-      return 'Een naam geven is verplicht';
+      return this.intl.t('constraints.mandatoryName');
     }
     if (this.duplicateNames.length > 0) {
-      return 'Deze naam is al eens gebruikt';
+      return this.intl.t('constraints.duplicateName');
     }
 
     if (this.name.length > NAME_INPUT_CHAR_LIMIT) {
