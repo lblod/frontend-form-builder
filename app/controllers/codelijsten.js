4a339c01f1d31760e72ebc1b3cb66bc904110867
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-01-11 11:34:02 +0100

    SFB-72: create a feature flag that will show the original codelist page and set it to be visible TRUE

--- app/controllers/codelijsten.js
+++ app/controllers/codelijsten.js
@@ -1,8 +1,10 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { service } from '@ember/service';
 
 export default class CodelijstenController extends Controller {
+  @service features;
   @tracked scheme;
 
   @action updateSelectedSchemeTo(scheme) {
