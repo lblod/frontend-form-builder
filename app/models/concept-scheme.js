628edc9f64a539427542082cc33d34f0b273de7c
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-01-12 13:02:38 +0100

    SFB-72: option to create a new public codelist

--- app/models/concept-scheme.js
+++ app/models/concept-scheme.js
@@ -3,9 +3,14 @@ import Model, { attr, hasMany } from '@ember-data/model';
 export default class ConceptSchemeModel extends Model {
   @attr uri;
   @attr preflabel;
+  @attr isPublic;
   @hasMany('concept', { inverse: null, async: true }) concepts;
 
   get label() {
     return this.preflabel;
+  }
+
+  get isPrivate() {
+    return !this.isPublic;
   }
 }
