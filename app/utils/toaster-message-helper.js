a31f23f600bf9f2566d1f32f98afe0699273081d
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-01-16 09:46:22 +0100

    SFB-72: create a warning toaster message helper and use it in the codelist controller

--- app/utils/toaster-message-helper.js
+++ app/utils/toaster-message-helper.js
@@ -8,3 +8,14 @@ export function showErrorToasterMessage(
     timeOut: timeInMs,
   });
 }
+
+export function showWarningToasterMessage(
+  toasterService,
+  message,
+  title = 'Opgelet',
+  timeInMs = 5000
+) {
+  toasterService.warning(message, title, {
+    timeOut: timeInMs,
+  });
+}
