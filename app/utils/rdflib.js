efbe37f822396a726c19fa92cf3ae69f3acc6333
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-01-17 13:34:25 +0100

    SFB-112: add missing nodes namespace

--- app/utils/rdflib.js
+++ app/utils/rdflib.js
@@ -5,3 +5,4 @@ export const RDF = new Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#');
 export const FORM = new Namespace('http://lblod.data.gift/vocabularies/forms/');
 export const SH = new Namespace('http://www.w3.org/ns/shacl#');
 export const EXT = new Namespace('http://mu.semte.ch/vocabularies/ext/');
+export const NODES = new Namespace('http://data.lblod.info/form-data/nodes/');
