38d4fa77ce93b1d4a2dfef46dc1399ebe0e3f552
Arne Kerremans <arnie@pop-os.localdomain>
2024-01-18 11:08:27 +0100

    first steps V2 change

--- app/components/rdf-form-fields/property-group-selector.js
+++ app/components/rdf-form-fields/property-group-selector.js
@@ -32,7 +32,7 @@ export default class PropertyGroupSelector extends InputFieldComponent {
   loadOptions() {
     const sourceGraph = this.args.graphs.sourceGraph;
     const propertyGroup = new namedNode(
-      'http://lblod.data.gift/vocabularies/forms/PropertyGroup'
+      'http://lblod.data.gift/vocabularies/forms/Section' //V2 
     );
 
     const allRawOptions = this.args.formStore
