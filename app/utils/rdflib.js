3777cca6c49b85dd8655feb94f9febb19128d531
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-01-22 18:57:25 +0100

    refactor: remove namespace DISPLAY from utils/rdflib and define it in the util method

--- app/utils/rdflib.js
+++ app/utils/rdflib.js
@@ -1,4 +1,3 @@
 import { Namespace } from 'rdflib';
 
-export const DISPLAY = new Namespace('http://lblod.data.gift/display-types/');
 export const EXT = new Namespace('http://mu.semte.ch/vocabularies/ext/');
