c010badf56105acb73b4e5dc3fec2844036d1d94
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-01-24 15:32:27 +0100

    SFFB-123: refactor to use other packages than @rdfjs

--- app/utils/shacl-validate-ttl-code.js
+++ app/utils/shacl-validate-ttl-code.js
@@ -1,32 +1,25 @@
-import { Validator } from 'shacl-engine';
-import rdfDataset from '@rdfjs/dataset';
-import rdfDataModel from '@rdfjs/data-model';
 import { getLocalFileContentAsText } from './get-local-file-content';
 import { ForkingStore } from '@lblod/ember-submission-form-fields';
 import { GRAPHS } from '../controllers/formbuilder/edit';
+import SHACLValidator from 'rdf-validate-shacl';
+import { DatasetFactory } from 'rdf-ext';
 
 export async function shaclValidateTtlCode(ttlCode) {
   const fieldValidatorTtl = await getLocalFileContentAsText(
     '/SHACL/field-validator.ttl'
   );
 
-  const shapeDataset = createDatasetForTtlCode(fieldValidatorTtl);
-  const dataDataset = createDatasetForTtlCode(ttlCode);
+  const shapeQuads = getAllStatementsForTtlCode(fieldValidatorTtl);
+  const dataQuads = getAllStatementsForTtlCode(ttlCode);
 
-  const validator = new Validator(shapeDataset, { factory: rdfDataModel });
-  const report = await validator.validate({ dataset: dataDataset });
+  const shapeDataset = new DatasetFactory().dataset(shapeQuads);
+  const dataDataset = new DatasetFactory().dataset(dataQuads);
+
+  const validator = new SHACLValidator(shapeDataset);
+  const report = validator.validate(dataDataset);
 
   console.log('report:', report);
   console.log(`conforms: ${report.conforms}`);
-}
-
-function createDatasetForTtlCode(ttlCode) {
-  const dataset = rdfDataset.dataset();
-  for (const quad of getAllStatementsForTtlCode(ttlCode)) {
-    dataset.add(quad);
-  }
-
-  return dataset;
 }
 
 function getAllStatementsForTtlCode(ttlCode) {
