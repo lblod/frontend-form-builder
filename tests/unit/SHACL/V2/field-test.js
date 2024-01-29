5c30c85038396679ac0a40f5782e8f51626c6fa7
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-01-29 15:44:42 +0100

    SFB-142: update predicate dc:isPartOf in field shape test

--- tests/unit/SHACL/V2/field-test.js
+++ tests/unit/SHACL/V2/field-test.js
@@ -9,11 +9,11 @@ module('Unit | SHACL | V2 | Field', function () {
         @prefix sh: <http://www.w3.org/ns/shacl#>.
         @prefix displayTypes: <http://lblod.data.gift/display-types/>.
         @prefix nodes: <http://data.lblod.info/form-data/nodes/>.
-        @prefix dc: <http://purl.org/dc/terms/> .
+
         nodes:24289e48-258f-4919-8c3e-5783a6acb4a4
             a form:Field;
             form:displayType displayTypes:defaultInput;
-            dc:isPartOf nodes:d7b33768-3723-4291-a7be-3d8a7d7cdbc1;
+            form:partOf nodes:d7b33768-3723-4291-a7be-3d8a7d7cdbc1;
             sh:name "Veldnaam";
             sh:order 2;
             sh:path nodes:e61f56db-6346-4a61-a75e-33e091789e40 .
@@ -45,10 +45,10 @@ module('Unit | SHACL | V2 | Field', function () {
         @prefix sh: <http://www.w3.org/ns/shacl#>.
         @prefix displayTypes: <http://lblod.data.gift/display-types/>.
         @prefix nodes: <http://data.lblod.info/form-data/nodes/>.
-        @prefix dc: <http://purl.org/dc/terms/> .
+
         nodes:24289e48-258f-4919-8c3e-5783a6acb4a4
             a form:Field;
-            dc:isPartOf nodes:d7b33768-3723-4291-a7be-3d8a7d7cdbc1;
+            form:partOf nodes:d7b33768-3723-4291-a7be-3d8a7d7cdbc1;
             sh:name "Veldnaam";
             sh:order 2;
             sh:path nodes:e61f56db-6346-4a61-a75e-33e091789e40 .
