2cfc44e8e561addb5c399c80c4d273c13120b06f
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-01-29 12:55:44 +0100

    SFB-140: add constant for max characters in a textArea

--- app/utils/constants.js
+++ app/utils/constants.js
@@ -1,3 +1,4 @@
 export const DESCRIPTION_NOT_USED_PLACEHOLDER = '';
 export const NAME_INPUT_CHAR_LIMIT = 120;
+export const DESCRIPTION_INPUT_CHAR_LIMIT = 500;
 export const PAGE_SIZE_TO_GET_ALL = 999;
