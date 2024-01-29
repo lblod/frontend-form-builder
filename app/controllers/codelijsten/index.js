40a4f4a1c6f36adce0bb1e73ccf8b0fc61f80f3e
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-01-29 09:32:34 +0100

    refactor: remove the methods/functions that where used with the old codelist

--- app/controllers/codelijsten/index.js
+++ app/controllers/codelijsten/index.js
@@ -1,27 +1,10 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
 import { service } from '@ember/service';
 
 export default class CodelijstenController extends Controller {
   @service features;
-  @tracked scheme;
 
   sort = '-preflabel';
   page = 0;
   size = 20;
-  //#region orignal codelijsten
-  @action updateSelectedSchemeTo(scheme) {
-    this.scheme = scheme;
-  }
-
-  get configurationCode() {
-    const jsonConfigCode = {
-      conceptScheme: this.scheme.uri,
-      searchEnabled: true,
-    };
-
-    return JSON.stringify(jsonConfigCode, null, 2).trim();
-  }
-  //#endregion
 }
