5d49e7f9944030839bebdd21098e2874aeb2030c
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-01-29 14:32:36 +0100

    SFB-143: add debounce time for an input to the constants

--- app/utils/constants.js
+++ app/utils/constants.js
@@ -2,3 +2,4 @@ export const DESCRIPTION_NOT_USED_PLACEHOLDER = '';
 export const NAME_INPUT_CHAR_LIMIT = 120;
 export const DESCRIPTION_INPUT_CHAR_LIMIT = 500;
 export const PAGE_SIZE_TO_GET_ALL = 999;
+export const INPUT_DEBOUNCE_MS = 500;
