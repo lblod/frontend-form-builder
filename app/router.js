037d60d42043a31b56f4eed658f0fe68e39fcd66
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-02-12 13:24:38 +0100

    SFB-163: create new formbuilder/new route

--- app/router.js
+++ app/router.js
@@ -15,6 +15,7 @@ Router.map(function () {
     this.route('edit', { path: '/:id/edit' });
   });
   this.route('formbuilder', function () {
+    this.route('new');
     this.route('edit', { path: '/:id/edit' }, function () {
       this.route('code');
       this.route('builder');
