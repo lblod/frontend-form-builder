df418b2ee1b203ea20a55f70f4df9feb1fa0458c
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-02-16 14:04:50 +0100

    SFB-172: add the conceptscheme data to the test form so it can show the options of a dropdown

--- app/controllers/forms/test.js
+++ app/controllers/forms/test.js
@@ -4,8 +4,12 @@ import { tracked } from '@glimmer/tracking';
 import { restartableTask } from 'ember-concurrency';
 import { ForkingStore } from '@lblod/ember-submission-form-fields';
 import { FORM, RDF } from '@lblod/submission-form-helpers';
+import { findTtlForUsedConceptSchemesInForm } from '../../utils/find-ttl-for-used-concept-schemes';
+import { service } from '@ember/service';
 
 export default class FormsTestController extends Controller {
+  @service store;
+
   @tracked form;
   @tracked formStore;
 
@@ -17,6 +21,20 @@ export default class FormsTestController extends Controller {
       this.model.graphs.formGraph,
       'text/turtle'
     );
+
+    const conceptSchemesTtl = await findTtlForUsedConceptSchemesInForm(
+      this.formStore,
+      this.store,
+      this.model.graphs.formGraph
+    );
+
+    if (conceptSchemesTtl) {
+      this.formStore.parse(
+        conceptSchemesTtl,
+        this.model.graphs.metaGraph,
+        'text/turtle'
+      );
+    }
 
     this.form = this.formStore.any(
       undefined,
