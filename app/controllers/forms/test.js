847fc47e30cc2189a6c6abd03eaf16bc297d2d42
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-02-16 13:23:31 +0100

    SFB-172: show the form in on the test page

--- app/controllers/forms/test.js
+++ app/controllers/forms/test.js
@@ -1,3 +1,28 @@
 import Controller from '@ember/controller';
 
-export default class FormsTestController extends Controller {}
+import { tracked } from '@glimmer/tracking';
+import { restartableTask } from 'ember-concurrency';
+import { ForkingStore } from '@lblod/ember-submission-form-fields';
+import { FORM, RDF } from '@lblod/submission-form-helpers';
+
+export default class FormsTestController extends Controller {
+  @tracked form;
+  @tracked formStore;
+
+  setupForm = restartableTask(async () => {
+    this.formStore = new ForkingStore();
+
+    this.formStore.parse(
+      this.model.formTtlCode,
+      this.model.graphs.formGraph,
+      'text/turtle'
+    );
+
+    this.form = this.formStore.any(
+      undefined,
+      RDF('type'),
+      FORM('Form'),
+      this.model.graphs.formGraph
+    );
+  });
+}
--- app/routes/forms/test.js
+++ app/routes/forms/test.js
@@ -1,3 +1,27 @@
 import Route from '@ember/routing/route';
 
-export default class FormsTestRoute extends Route {}
+import {
+  GRAPHS,
+  PREVIEW_SOURCE_NODE,
+} from '../../controllers/formbuilder/edit';
+import { service } from '@ember/service';
+
+export default class FormsTestRoute extends Route {
+  @service store;
+
+  async model(params) {
+    const form = await this.store.findRecord('generated-form', params.id);
+
+    return {
+      formTtlCode: form.ttlCode,
+      graphs: GRAPHS,
+      sourceNode: PREVIEW_SOURCE_NODE,
+    };
+  }
+
+  setupController(controller) {
+    super.setupController(...arguments);
+
+    controller.setupForm.perform();
+  }
+}
