86795d5cca5c4c5e453136648651438eccaccfa5
Arne Kerremans <arnie@pop-os.localdomain>
2024-02-19 13:41:37 +0100

    go back to formbuilder goes back right form

--- app/controllers/codelijsten/index.js
+++ app/controllers/codelijsten/index.js
@@ -1,10 +1,21 @@
 import Controller from '@ember/controller';
 import { service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class CodelijstenController extends Controller {
   @service features;
+  @service router;
 
   sort = '-preflabel';
   page = 0;
   size = 20;
+
+  @action
+  returnToFormBuilder() {
+    if (this.formId) {
+      this.router.transitionTo('formbuilder.edit', this.formId);
+    } else {
+      this.router.transitionTo('index');
+    }
+  }
 }
