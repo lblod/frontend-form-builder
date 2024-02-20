ae364840f2cb0ac883be45ecb85ffd39af476301
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-02-20 15:43:44 +0100

    log: remove log in RT editor component

--- app/components/rdf-form-fields/rich-text-editor.js
+++ app/components/rdf-form-fields/rich-text-editor.js
@@ -31,7 +31,6 @@ export default class RichTextEditorComponent extends SimpleInputFieldComponent {
   plugins = [];
 
   nodeViews = (controller) => {
-    console.log(`nodeviews`, controller);
     return {
       link: linkView(this.linkOptions)(controller),
     };
