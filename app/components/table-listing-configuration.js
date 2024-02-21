801806f138351972c8ba4b8f89b52b9eed6a2d66
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-02-21 17:29:42 +0100

    SFB-179: get all tables uris

--- app/components/table-listing-configuration.js
+++ app/components/table-listing-configuration.js
@@ -1,9 +1,32 @@
 import Component from '@glimmer/component';
 
+import { service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import { ForkingStore } from '@lblod/ember-submission-form-fields';
+import { GRAPHS } from '../controllers/formbuilder/edit';
+import { RDF, FORM, SHACL } from '@lblod/submission-form-helpers';
+
 export default class TableListingConfigurationComponent extends Component {
+  @service('form-code-manager') formCodeManager;
+  @tracked tables;
+
   constructor() {
     super(...arguments);
+    const tableListingUris = this.getTableListingUris();
+    console.log({ tableListingUris });
+  }
 
-    console.log(this.args);
+  getTableListingUris() {
+    const ttlCode = this.formCodeManager.getTtlOfLatestVersion();
+    const store = new ForkingStore();
+    store.parse(ttlCode, GRAPHS.sourceGraph, 'text/turtle');
+
+    return store
+      .match(undefined, RDF('type'), FORM('ListingTable'))
+      .map((triple) => triple.subject);
+  }
+
+  getSectionTitleAndColumnsForTables(tableUris) {
+
   }
 }
