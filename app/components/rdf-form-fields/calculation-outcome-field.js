2e14bfd4fdf5ebfd797d370d429daf40ae0af293
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-02-21 12:45:53 +0100

    SFB-179: create the scope statements and see ttlCode WIP

--- app/components/rdf-form-fields/calculation-outcome-field.js
+++ app/components/rdf-form-fields/calculation-outcome-field.js
@@ -1,7 +1,67 @@
 import Component from '@glimmer/component';
+import { EXT } from '../../utils/namespaces';
+import { RDF, FORM, SHACL } from '@lblod/submission-form-helpers';
+import { Statement } from 'rdflib';
 
 export default class CalculationOutcomeFieldComponent extends Component {
   constructor() {
     super(...arguments);
+    console.log('arguments', this.args);
+    this.addScopeToTtl();
+  }
+
+  addScopeToTtl() {
+    this.store.addAll(this.createScopeStatements());
+    console.log(`ttlCode after adding the scope`, this.ttlCode);
+  }
+
+  createScopeStatements() {
+    const subject = EXT(this.scopeName);
+    const type = new Statement(
+      subject,
+      RDF('type'),
+      FORM('scope'),
+      this.graphs.sourceGraph
+    );
+    const path = new Statement(
+      subject,
+      SHACL('path'),
+      this.scopePath,
+      this.graphs.sourceGraph
+    );
+
+    return [type, path];
+  }
+
+  get ttlCode() {
+    return this.store.serializeDataMergedGraph(this.graphs.sourceGraph);
+  }
+
+  get scopePath() {
+    return EXT('outcome');
+  }
+
+  get scopeName() {
+    return `${this.fieldId}-outcomeS`;
+  }
+
+  get fieldId() {
+    const url = this.field.uri.value;
+    const parts = url.split('/');
+    const id = parts[parts.length - 1];
+
+    return id;
+  }
+
+  get store() {
+    return this.args.formStore;
+  }
+
+  get graphs() {
+    return this.args.graphs;
+  }
+
+  get field() {
+    return this.args.field;
   }
 }
