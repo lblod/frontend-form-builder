cc79c35da407e5c82af4afd9176c807c9f113f43
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-02-22 14:33:47 +0100

    SFB-179: can slect table column and action  + log selected

--- app/components/table-listing-configuration.js
+++ app/components/table-listing-configuration.js
@@ -4,12 +4,17 @@ import { service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 import { ForkingStore } from '@lblod/ember-submission-form-fields';
 import { GRAPHS } from '../controllers/formbuilder/edit';
-import { RDF, FORM, SHACL } from '@lblod/submission-form-helpers';
+import { RDF, FORM } from '@lblod/submission-form-helpers';
 import { getMinimalNodeInfo } from '../utils/forking-store-helpers';
+import { action } from '@ember/object';
+import { sortObjectsOnProperty } from '../utils/sort-object-on-property';
 
 export default class TableListingConfigurationComponent extends Component {
   @service('form-code-manager') formCodeManager;
   @tracked tables;
+  @tracked selectedTable;
+  @tracked selectedColumn;
+  @tracked selectedColumnAction;
 
   store;
 
@@ -51,16 +56,77 @@ export default class TableListingConfigurationComponent extends Component {
         .map((triple) => triple.object);
 
       return {
-        sectionTitle: getMinimalNodeInfo(
+        section: getMinimalNodeInfo(
           sectionNode,
           this.store,
           this.graphs.sourceGraph
-        ).name,
-        columns: columnSubjects.map((columnNode) =>
-          getMinimalNodeInfo(columnNode, this.store, this.graphs.sourceGraph)
         ),
+        columns: columnSubjects.map((columnNode) => {
+          return {
+            ...getMinimalNodeInfo(
+              columnNode,
+              this.store,
+              this.graphs.sourceGraph
+            ),
+          };
+        }),
       };
     });
+  }
+
+  @action
+  setTable(table) {
+    this.selectedTable = table;
+  }
+
+  @action
+  setColumnForTable(column) {
+    this.selectedColumn = column;
+  }
+
+  @action
+  setColumnAction(action) {
+    this.selectedColumnAction = action;
+    console.log(
+      `set action '${action.label}' on column '${this.selectedColumn.name}' for table '${this.selectedTable.name}'`
+    );
+  }
+
+  get sortedTables() {
+    return this.tables; // sort them on order, section and columns TODO:
+  }
+
+  get tableOptions() {
+    return sortObjectsOnProperty(
+      this.tables.map((table) => {
+        return {
+          subject: table.section.subject,
+          name: table.section.name,
+          order: table.section.order,
+        };
+      }),
+      'order'
+    );
+  }
+
+  get columnOptions() {
+    if (!this.selectedTable) {
+      return [];
+    }
+
+    const table = this.tables.find(
+      (table) => table.section.subject == this.selectedTable.subject
+    );
+
+    if (!table) {
+      return [];
+    }
+
+    return sortObjectsOnProperty(table.columns, 'order');
+  }
+
+  get columnActions() {
+    return [{ label: 'Geen actie' }, { label: 'Som' }];
   }
 
   get graphs() {
