78ce9dd9f77380245f3517b3ad65305b0114b295
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-02-22 10:28:34 +0100

    SFB-179: get tables with their columns

--- app/components/table-listing-configuration.js
+++ app/components/table-listing-configuration.js
@@ -5,28 +5,65 @@ import { tracked } from '@glimmer/tracking';
 import { ForkingStore } from '@lblod/ember-submission-form-fields';
 import { GRAPHS } from '../controllers/formbuilder/edit';
 import { RDF, FORM, SHACL } from '@lblod/submission-form-helpers';
+import { getMinimalNodeInfo } from '../utils/forking-store-helpers';
 
 export default class TableListingConfigurationComponent extends Component {
   @service('form-code-manager') formCodeManager;
   @tracked tables;
 
+  store;
+
   constructor() {
     super(...arguments);
-    const tableListingUris = this.getTableListingUris();
-    console.log({ tableListingUris });
+    this.setupStore();
+    this.setSectionTitleAndColumnsForTables(this.getTableListingUris());
+  }
+
+  setupStore() {
+    const ttlCode = this.formCodeManager.getTtlOfLatestVersion();
+    this.store = new ForkingStore();
+    this.store.parse(ttlCode, this.graphs.sourceGraph, 'text/turtle');
   }
 
   getTableListingUris() {
-    const ttlCode = this.formCodeManager.getTtlOfLatestVersion();
-    const store = new ForkingStore();
-    store.parse(ttlCode, GRAPHS.sourceGraph, 'text/turtle');
-
-    return store
+    return this.store
       .match(undefined, RDF('type'), FORM('ListingTable'))
       .map((triple) => triple.subject);
   }
 
-  getSectionTitleAndColumnsForTables(tableUris) {
+  setSectionTitleAndColumnsForTables(tableUris) {
+    this.tables = tableUris.map((tableListing) => {
+      const sectionNode = this.store.any(
+        tableListing,
+        FORM('partOf'),
+        undefined,
+        this.graphs.sourceGraph
+      );
 
+      const subform = this.store.any(
+        tableListing,
+        FORM('each'),
+        undefined,
+        this.graphs.sourceGraph
+      );
+      const columnSubjects = this.store
+        .match(subform, FORM('includes'), undefined, this.graphs.sourceGraph)
+        .map((triple) => triple.object);
+
+      return {
+        sectionTitle: getMinimalNodeInfo(
+          sectionNode,
+          this.store,
+          this.graphs.sourceGraph
+        ).name,
+        columns: columnSubjects.map((columnNode) =>
+          getMinimalNodeInfo(columnNode, this.store, this.graphs.sourceGraph)
+        ),
+      };
+    });
+  }
+
+  get graphs() {
+    return GRAPHS;
   }
 }
