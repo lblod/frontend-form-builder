cc9435f4715f906bca4748833a0932afa2a3aed6
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-02-23 12:02:24 +0100

    SFB-179: create start for table listing configuration with form generators

---
+++ app/components/table-listing-configuration-with-form.js
@@ -0,0 +1,8 @@
+import Component from '@glimmer/component';
+
+export default class TableListingConfigurationWithFormComponent extends Component {
+  constructor() {
+    super(...arguments);
+    console.log(`| TableListingConfigurationWithFormComponent |`);
+  }
+}
