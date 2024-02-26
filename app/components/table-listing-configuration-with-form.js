fba25ac9307cdb89c46ea9ea71ab815426d516f0
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-02-26 09:42:47 +0100

    SFB:179: remove the second idea component config from form

--- app/components/table-listing-configuration-with-form.js
+++
@@ -1,8 +0,0 @@
-import Component from '@glimmer/component';
-
-export default class TableListingConfigurationWithFormComponent extends Component {
-  constructor() {
-    super(...arguments);
-    console.log(`| TableListingConfigurationWithFormComponent |`);
-  }
-}
