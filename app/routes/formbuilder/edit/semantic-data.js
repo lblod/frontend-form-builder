28a5b1f5a9174d5d3b7537f9866d4d7239dd1069
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-02-27 11:51:26 +0100

    SFB-190: route to the code tab when no ttl is set for the form

--- app/routes/formbuilder/edit/semantic-data.js
+++ app/routes/formbuilder/edit/semantic-data.js
@@ -3,12 +3,23 @@ import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 
 export default class FormbuilderEditSemanticDataRoute extends Route {
+  @service router;
   @service('form-code-manager') formCodeManager;
 
   async model() {
-    return {
-      ttlCode: this.formCodeManager.getTtlOfLatestVersion(),
-    };
+    try {
+      const ttl = this.formCodeManager.getTtlOfLatestVersion();
+
+      return {
+        ttlCode: ttl,
+      };
+    } catch (error) {
+      console.log(this.formCodeManager.getFormId());
+      this.router.transitionTo(
+        'formbuilder.edit',
+        this.formCodeManager.getFormId()
+      );
+    }
   }
 
   setupController(controller, model) {
