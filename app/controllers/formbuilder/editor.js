d416946b69291ff5f6084e61de2ac17d8c696f9b
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-02-29 15:37:21 +0100

    SFB-199: show the basic elements in the left sidebar

--- app/controllers/formbuilder/editor.js
+++ app/controllers/formbuilder/editor.js
@@ -1,8 +1,34 @@
 import Controller from '@ember/controller';
+import { sortObjectsOnProperty } from '../../utils/sort-object-on-property';
 
 export default class FormbuilderEditorController extends Controller {
   constructor() {
     super(...arguments);
     console.log(`Editor | controller`);
+  }
+
+  get sortedBasicElements() {
+    return sortObjectsOnProperty(this.basicElements, 'order');
+  }
+
+  get basicElements() {
+    return [
+      {
+        label: 'Sectie',
+        order: 1,
+      },
+      {
+        label: 'Veld',
+        order: 2,
+      },
+      {
+        label: 'Listing',
+        order: 3,
+      },
+      {
+        label: 'Tabel',
+        order: 4,
+      },
+    ];
   }
 }
