09e36c609a436eb45002ca4dd236801e79fdab5b
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-03-04 11:23:13 +0100

    SFB-179: register as custom component

--- app/routes/formbuilder/edit.js
+++ app/routes/formbuilder/edit.js
@@ -11,6 +11,7 @@ import { GRAPHS } from '../../controllers/formbuilder/edit';
 import ErrorMessageInputFieldComponent from '../../components/rdf-form-fields/error-message-input-field';
 import { showErrorToasterMessage } from '../../utils/toaster-message-helper';
 import RichTextEditorComponent from '../../components/rdf-form-fields/rich-text-editor';
+import AggregateFieldComponent from '../../components/rdf-form-fields/aggregate-field';
 
 export default class FormbuilderEditRoute extends Route {
   @service store;
@@ -113,6 +114,10 @@ export default class FormbuilderEditRoute extends Route {
       {
         displayType: 'http://lblod.data.gift/display-types/richTextEditor',
         edit: RichTextEditorComponent,
+      },
+      {
+        displayType: 'http://lblod.data.gift/display-types/aggregateField',
+        edit: AggregateFieldComponent,
       },
     ]);
   }
