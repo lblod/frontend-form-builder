7460a14271ecc52d5ec3e879c0c52d29386e2427
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-03-04 14:56:07 +0100

    SFB-202: show the inputfiled display type component in the form

--- app/components/editor/blocks/field.js
+++ app/components/editor/blocks/field.js
@@ -1,6 +1,7 @@
 import Component from '@glimmer/component';
 
-import { SHACL } from '@lblod/submission-form-helpers';
+import { SHACL, FORM } from '@lblod/submission-form-helpers';
+import { DISPLAY } from '../../../utils/namespaces';
 
 export default class EditorBlocksFieldComponent extends Component {
   get fieldName() {
@@ -15,7 +16,23 @@ export default class EditorBlocksFieldComponent extends Component {
     return foundName.object.value;
   }
 
+  get displayTypeUri() {
+    const foundType = this.data.statements.find(
+      (st) => st.predicate.value == FORM('displayType').value
+    );
+    if (!foundType) {
+      console.log(`No displaytype found for field`, this.subject);
+      return null;
+    }
+
+    return foundType.object.value;
+  }
+
   get data() {
     return this.args.data;
+  }
+
+  get isDefaultInput() {
+    return this.displayTypeUri == DISPLAY('defaultInput').value;
   }
 }
--- app/utils/namespaces.js
+++ app/utils/namespaces.js
@@ -1,3 +1,4 @@
 import { Namespace } from 'rdflib';
 
 export const EXT = new Namespace('http://mu.semte.ch/vocabularies/ext/');
+export const DISPLAY = new Namespace('http://lblod.data.gift/display-types/');
