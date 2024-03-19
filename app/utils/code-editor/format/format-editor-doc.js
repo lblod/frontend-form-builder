9c2584b2f69611501171f574b52ae3eaa369417d
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-03-19 11:12:51 +0100

    SFB-223: do not use the separate util methods start doing it one

--- app/utils/code-editor/format/format-editor-doc.js
+++ app/utils/code-editor/format/format-editor-doc.js
@@ -1,23 +1,34 @@
-import { getDocWithNewLineAfterDots } from './rules/add-new-line-after-dot';
-import { getDocWithTabBeforeLineEndingOnDotAndPreviousLineEndsWithSemiColon } from './rules/add-tab-before-line-ending-with-dot-and-previous-with-semicolon';
-import { getDocWithTabBeforeLineWithSemicolon } from './rules/add-tab-before-line-with-semicolon';
-import { getDocWhereAllIncludesAreTabbed } from './rules/add-tab-before-item-that-is-included';
-import { getDocWithFormattedValidations } from './rules/format-blankNode-validations';
-import { getDocWithParsedLinesThatEndOnDotAndIncludeSemicolons } from './rules/parse-line-when-ending-on-dot-and-semicolon-included';
-import { getDocWithAllSpacesRemovedFromEachLine } from './rules/remove-all-spaces-from-each-line';
-
 export async function getFormattedEditorCode(doc) {
-  // Watch out with the order
-  doc = await getDocWithAllSpacesRemovedFromEachLine(doc);
-  doc = await getDocWhereAllIncludesAreTabbed(doc);
-  doc =
-    await getDocWithTabBeforeLineEndingOnDotAndPreviousLineEndsWithSemiColon(
-      doc
-    );
-  doc = await getDocWithNewLineAfterDots(doc);
-  doc = await getDocWithTabBeforeLineWithSemicolon(doc);
-  doc = await getDocWithFormattedValidations(doc);
-  doc = await getDocWithParsedLinesThatEndOnDotAndIncludeSemicolons(doc);
+  console.log(`doc`, doc);
+  if (isTextLeaf(doc)) {
+    return formatTextLeaf(doc);
+  }
+  if (isTextNode(doc)) {
+    const newDocument = [];
+    for (const textLeaf of doc.children) {
+      const formattedLeaf = formatTextLeaf(textLeaf);
+      newDocument.push(...formattedLeaf);
+    }
+    return newDocument;
+  }
+}
 
-  return await getDocWithNewLineAfterDots(doc); // this rule is done twice because all the rules before could have changed this "a nice finisher"
+function isTextLeaf(doc) {
+  return doc.text ? true : false;
+}
+
+function isTextNode(doc) {
+  return doc.children ? true : false;
+}
+
+function formatTextLeaf(textLeaf) {
+  if (!isTextLeaf(textLeaf)) {
+    return [];
+  }
+  const formattedTextLeaf = [];
+  for (const line of textLeaf.text) {
+    console.log(`line`, line);
+  }
+
+  return formattedTextLeaf;
 }
