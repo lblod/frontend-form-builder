be3d6e678882f8934e231df3df9a8c20d8a25eb6
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-03-26 10:27:58 +0100

    SFB-229: get the possible validation types for the field display type

--- app/routes/formbuilder/edit/validations.js
+++ app/routes/formbuilder/edit/validations.js
@@ -1,13 +1,20 @@
 import Route from '@ember/routing/route';
+import { getLocalFileContentAsText } from '../../../utils/get-local-file-content';
 
 export default class FormbuilderEditValidationsRoute extends Route {
-  model() {
+  async model() {
     const editRoute = this.modelFor('formbuilder.edit');
     /* eslint ember/no-controller-access-in-routes: "warn" */
     const editController = this.controllerFor('formbuilder.edit');
 
+    const validationsTtl = await getLocalFileContentAsText(
+      '/forms/validation/meta.ttl'
+    );
+    const fieldTtl = await getLocalFileContentAsText('/forms/builder/meta.ttl');
+
     return {
       graphs: editRoute.graphs,
+      validationsTtl: `${validationsTtl} ${fieldTtl}`,
       handleCodeChange: editController.handleCodeChange,
     };
   }
