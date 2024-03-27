30efb7d1849144bd016b4d324a97b1d856fe8c59
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-03-27 08:58:29 +0100

    SFB-229: can show and visually toggle the custom and default error message

--- app/components/validation/result-message.js
+++ app/components/validation/result-message.js
@@ -2,20 +2,43 @@ import Component from '@glimmer/component';
 
 import { guidFor } from '@ember/object/internals';
 import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class ValidationResultMessageComponent extends Component {
   inputId = 'validation-result-message-' + guidFor(this);
 
   @tracked message;
+  @tracked isUsingCustomMessage;
 
   constructor() {
     super(...arguments);
 
-    this.message = '';
     console.log(`config`, this.args.validationConfig);
+
+    this.message = '';
     if (this.args.validationConfig.resultMessage) {
       const { resultMessage } = this.args.validationConfig;
       this.message = resultMessage.object.value;
     }
+  }
+
+  @action
+  setIsUsingCustomMessage(state) {
+    this.isUsingCustomMessage = state;
+  }
+
+  @action
+  updateMessage(event) {
+    const inputValue = event.target.value;
+
+    if (!inputValue) {
+      this.message = '';
+    } else {
+      this.message = inputValue.trim();
+    }
+  }
+
+  get hasErrors() {
+    return false;
   }
 }
