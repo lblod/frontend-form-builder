fef558a25cbe2c022d1cda0a15f2350b6143c911
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2024-04-04 09:24:36 +0200

    SFB-246: add order as an attribute on the concept model

--- app/models/concept.js
+++ app/models/concept.js
@@ -1,9 +1,13 @@
 import Model, { attr, hasMany } from '@ember-data/model';
 import { RDF, SKOS } from '@lblod/submission-form-helpers';
+import { Namespace } from 'rdflib';
+
+const QB = new Namespace('http://purl.org/linked-data/cube#');
 
 export default class ConceptModel extends Model {
   @attr uri;
   @attr preflabel;
+  @attr order;
   @hasMany('concept-scheme', { inverse: null, async: true }) conceptSchemes;
 
   get label() {
@@ -15,6 +19,7 @@ export default class ConceptModel extends Model {
       <${this.uri}>
       ${RDF('type')} ${SKOS('Concept')} ;
       ${SKOS('prefLabel')} "${this.label}" ;
+      ${QB('order')} ${this.order} ;
       ${SKOS('inScheme')} <${conceptSchemeUri}> .
     `;
   }
